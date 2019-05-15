872b6cc53659af9181cc52cc939df3440b9e066d
Guilherme T Maeoka <gui.mspace@gmail.com>
2019-05-15 09:57:40 -0300

    Add TRANSPOSE function in BSSUMBYTAG

--- src/setupAddon.js
+++ src/setupAddon.js
@@ -638,7 +638,7 @@ function setupPart4_(spreadsheet, listNameMonths, number_accounts) {
 
       formula = "{\"" + listNameMonthsFull[i] + "\"; ";
       formula += "IF(\'_Settings\'!$B$7 > 0; ";
-      formula += "BSSUMBYTAG($D$1:$D; FILTER(" + rg + "; ";
+      formula += "BSSUMBYTAG(TRANSPOSE($D$1:$D); FILTER(" + rg + "; ";
       formula += "NOT(ISBLANK(" + cd + ")))); )}";
 
       vFormulas[0].push(formula);
