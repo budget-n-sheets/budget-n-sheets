ecf6e4cfe484c0ccaedc3d1e430565993d27b445
Guilherme T Maeoka <gui.mspace@gmail.com>
2019-05-26 16:55:13 -0300

    Bump add-on version to 0.17.0-beta5

--- src/head.js
+++ src/head.js
@@ -3,7 +3,7 @@ var AppsScriptGlobal = (function() {
     test_chamber: false,
 
     AddonVersion: 54,
-    AddonVersionName: "0.17.0-beta4",
+    AddonVersionName: "0.17.0-beta5",
 
     TemplateId: "",
     TemplateVersion: 50,
