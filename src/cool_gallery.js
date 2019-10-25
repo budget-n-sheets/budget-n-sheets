fab88f3b156804fa94e7fc95dfbe9982a49d6e58
Guilherme T Maeoka <gui.mspace@gmail.com>
2019-10-25 13:47:45 -0300

    Add double test for word or bool true

--- src/cool_gallery.js
+++ src/cool_gallery.js
@@ -86,7 +86,7 @@ function coolTags(info) {
 	sheet.getRange('E2').setFormula('\'_Settings\'!B4');
 	sheet.getRange('E3').setFormula('\'_Settings\'!B5');
 
-	sheet.getRange('B6').setFormula('QUERY({Tags!$B$1:$T}; "select Col1, sum(Col5), sum(Col6), sum(Col7), sum(Col8), sum(Col9), sum(Col10), sum(Col11), sum(Col12), sum(Col13), sum(Col14), sum(Col15), sum(Col16), sum(Col18), sum(Col19) where Col3 = \'TRUE\' group by Col1"; 1)');
+	sheet.getRange('B6').setFormula('QUERY({Tags!$B$1:$T}; "select Col1, sum(Col5), sum(Col6), sum(Col7), sum(Col8), sum(Col9), sum(Col10), sum(Col11), sum(Col12), sum(Col13), sum(Col14), sum(Col15), sum(Col16), sum(Col18), sum(Col19) where Col3=true or Col3=\'TRUE\' group by Col1"; 1)');
 
 	chart = sheet.newChart()
 		.addRange( sheet.getRange('B18:N28') )
