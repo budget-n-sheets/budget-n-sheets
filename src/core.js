2631da6edc31867c4dd0dc0f989d9bd74f4b1473
Guilherme T Maeoka <gui.mspace@gmail.com>
2020-01-02 12:08:32 -0300

    Remove test suite trial_onOpen() from core

--- src/core.js
+++ src/core.js
@@ -20,13 +20,6 @@ function onInstall(e) {
 	* @param {Object} e The event parameter for a simple onOpen trigger.
 	*/
 function onOpen(e) {
-
-	try {
-		trial_onOpen();
-	} catch (err) {
-
-	}
-
 	if (e && e.authMode == ScriptApp.AuthMode.NONE) {
 		SpreadsheetApp.getUi()
 			.createAddonMenu()
