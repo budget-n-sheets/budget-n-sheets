def73d041b9fbd9cbf6093da3891d1e5cd23430a
Guilherme T Maeoka <gui.mspace@gmail.com>
2020-05-08 16:43:49 -0300

    Add v2 of cards and tables methods

---
+++ src/cards2.js
@@ -0,0 +1,182 @@
+function getCardById_(card_id) {
+	const db_tables = getPropertiesService_("document", "json", "DB_TABLES");
+
+	var c = db_tables.cards.ids.indexOf(card_id);
+	if (c !== -1) return db_tables.cards.data[c];
+
+	console.warn("getCardById_(): Card was not found.");
+}
+
+
+function addCard_(card) {
+	var aliases, c;
+
+	if (! /^\w+$/.test(card.code)) return 1;
+
+	const db_tables = getPropertiesService_("document", "json", "DB_TABLES");
+	const db_cards = db_tables.cards;
+
+	if (db_cards.count >= 10) return 1;
+	if (db_cards.codes.indexOf(card.code) !== -1) return 1;
+
+	aliases = card.aliases.match(/\w+/g);
+	if (aliases == null) aliases = [ ];
+
+	c = aliases.indexOf(input.code);
+	while (c !== -1) {
+		aliases.splice(c, 1);
+		c = aliases.indexOf(input.code);
+	}
+
+	card.id = randomString(7, "lonum");
+	card.aliases = aliases;
+	card.limit = Number(card.limit);
+
+	c = db_cards.count++;
+
+	db_cards.ids[c] = card.id;
+	db_cards.codes[c] = card.code;
+	db_cards.data[c] = card;
+
+	db_tables.cards = db_cards;
+	setPropertiesService_("document", "json", "DB_TABLES", db_tables);
+}
+
+
+function setCard_(card) {
+	var aliases;
+	var pos, c, i;
+
+	if (! /^\w+$/.test(card.code)) return 1;
+
+	const db_tables = getPropertiesService_("document", "json", "DB_TABLES");
+	const db_cards = db_tables.cards;
+
+	pos = db_cards.ids.indexOf(card.id);
+	if (pos === -1) return 1;
+
+	for (i = 0; i < db_cards.codes.length; i++) {
+		if (i !== pos && db_cards.codes[i] === card.code) return 1;
+	}
+
+	aliases = card.aliases.match(/\w+/g);
+	if (aliases == null) aliases = [ ];
+
+	c = aliases.indexOf(card.code);
+	while (c !== -1) {
+		aliases.splice(c, 1);
+		c = aliases.indexOf(card.code);
+	}
+
+	db_cards.codes[pos] = card.code;
+
+	db_cards.data[pos] = {
+		id: card.id,
+		name: card.name,
+		code: card.code,
+		aliases: aliases,
+		limit: Number(card.limit)
+	};
+
+	db_tables.cards = db_cards;
+	setPropertiesService_("document", "json", "DB_TABLES", db_tables);
+}
+
+
+function deleteCard_(card_id) {
+	var c;
+
+	const db_tables = getPropertiesService_("document", "json", "DB_TABLES");
+	const db_cards = db_tables.cards;
+
+	c = db_cards.ids.indexOf(input);
+	if (c === -1) return 1;
+
+	db_cards.count--;
+	db_cards.ids.splice(c, 1);
+	db_cards.codes.splice(c, 1);
+	db_cards.data.splice(c, 1);
+
+	db_tables.cards = db_cards;
+	setPropertiesService_("document", "json", "DB_TABLES", db_tables);
+}
+
+
+function refreshCardsRules_() {
+	var sheet, ranges, rule1, rule2;
+	var card, list1, list2;
+	var text, col, n1, i, j, k;
+
+	sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("_Backstage");
+	if (!sheet) return;
+
+	const h_ = TABLE_DIMENSION.height;
+	const w_ = TABLE_DIMENSION.width;
+
+	const num_acc = getConstProperties_("number_accounts");
+
+	const db_cards = getPropertiesService_("document", "obj", "DB_TABLES");
+	db_cards = db_cards.cards;
+
+	col = 2 + w_ + w_*num_acc;
+
+	list1 = [ "All" ];
+	list2 = [ ];
+
+	sheet.getRange(1, col, 1, 11*w_).setValue("");
+	sheet.getRange(1, col).setValue("All");
+
+	col += w_;
+
+	for (i = 0; i < db_cards.count; i++) {
+		card = db_cards.data[i];
+
+		list1.push(card.code);
+		list2.push(card.code);
+
+		for (j = 0; j < card.aliases.length; j++) {
+			list2.push(card.aliases[j]);
+		}
+
+		ranges = [ ];
+		for (j = 0; j < 12; j++) {
+			ranges[j] = rollA1Notation(2 + h_*j, 1 + col + w_*i);
+		}
+
+		text = "^" + card.code + "$";
+		for (j = 0; j < card.aliases.length; j++) {
+			text += "|^" + card.aliases[j] + "$";
+		}
+
+		sheet.getRange(1, col + w_*i).setValue(text);
+		sheet.getRangeList(ranges).setValue("=" + Number(card.limit).formatLocaleSignal());
+	}
+
+	sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Cards");
+	if (!sheet) return;
+
+	n1 = sheet.getMaxRows() - 5;
+	if (n1 < 1) return;
+
+	rule1 = SpreadsheetApp.newDataValidation()
+						.requireValueInList(list1, true)
+						.setAllowInvalid(true)
+						.build();
+
+	rule2 = SpreadsheetApp.newDataValidation()
+						.requireValueInList(list2, true)
+						.setAllowInvalid(true)
+						.build();
+
+	for (i = 0; i < 12; i++) {
+		sheet.getRange(2, 2 + 6*i)
+			.clearDataValidations()
+			.setDataValidation(rule1);
+
+		sheet.getRange(6, 3 + 6*i, n1, 1)
+			.clearDataValidations()
+			.setDataValidation(rule2);
+	}
+
+	SpreadsheetApp.flush();
+}
---
+++ src/tables2.js
@@ -0,0 +1,182 @@
+function tablesService(action, select, param) {
+	switch (action) {
+	case "get":
+		return getTablesService_(select, param);
+	case "set":
+		return setTablesService_(select, param);
+	case "refresh":
+		return refreshTablesService_(select, param);
+
+	default:
+		consoleLog_("warn", "tablesService(): Switch case is default.", action);
+		return 1;
+	}
+}
+
+function getTablesService_(select, param) {
+	switch (select) {
+	case "all":
+		return getTables_(param);
+	case "account":
+		return getAccountById_(param);
+	case "card":
+		return getCardById_(param);
+
+	default:
+		consoleLog_("warn", "getTablesService_(): Switch case is default.", select);
+		return 1;
+	}
+}
+
+function setTablesService_(select, param) {
+	var lock = LockService.getDocumentLock();
+	try {
+		lock.waitLock(2000);
+	} catch (err) {
+		consoleLog_("warn", "setTablesService_(): Wait lock time out.", err);
+		return 1;
+	}
+
+	switch (select) {
+	case "account":
+		return setAccount_(param);
+	case "addcard":
+		return addCard_(param);
+	case "setcard":
+		return setCard_(param);
+	case "deletecard":
+		return deleteCard_(param);
+
+	default:
+		consoleLog_("warn", "setTablesService_(): Switch case is default.", select);
+		return 1;
+	}
+}
+
+function refreshTablesService_(select, param) {
+	switch (select) {
+	case "accountname":
+		return refreshAccountName_(param);
+	case "cashflow":
+		return refreshCashFlowReferences_();
+	case "cardsrules":
+		return refreshCardsRules_();
+
+	default:
+		consoleLog_("warn", "refreshTablesService_(): Switch case is default.", select);
+		return 1;
+	}
+}
+
+
+function getTables_() {
+	const db_tables = getPropertiesService_("document", "json", "DB_TABLES");
+
+	var db = {
+		accounts: db_tables.accounts.data,
+		cards: db_tables.cards.data
+	};
+
+	return db;
+}
+
+
+function getAccountById_(acc_id) {
+	const db_tables = getPropertiesService_("document", "json", "DB_TABLES");
+
+	var c = db_tables.accounts.ids.indexOf(acc_id);
+	if (c !== -1) return db_tables.accounts.data[c];
+
+	console.warn("getAccountById_(): Account was not found.");
+}
+
+
+function setAccount_(account) {
+	var prev_time_a, c;
+
+	const db_tables = getPropertiesService_("document", "json", "DB_TABLES");
+	const db_accounts = db_tables.accounts;
+
+	c = db_accounts.ids.indexOf(account.id);
+	if (c === -1) return 1;
+
+	prev_time_a = account.time_a;
+
+	account.time_a = Number(account.time_a);
+	account.balance = Number(account.balance);
+
+	db_accounts.names[c] = account.name;
+
+	db_accounts.data[c].name = account.name;
+	db_accounts.data[c].time_a = account.time_a;
+	db_accounts.data[c].balance = account.balance;
+
+	db_tables.accounts = db_accounts;
+	setPropertiesService_("document", "json", "DB_TABLES", db_tables);
+
+	refreshAccountName_(c, account, prev_time_a);
+	refreshCashFlowReferences_();
+}
+
+
+function refreshAccountName_(c, account, prev_time_a) {
+	var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+	var sheet = spreadsheet.getSheetByName("_Backstage");
+	var spreadsheet, sheet, formula;
+
+	if (!sheet) return 1;
+
+	const h_ = TABLE_DIMENSION.height;
+	const w_ = TABLE_DIMENSION.width;
+
+	const col = 2 + w_ + w_*c;
+	const time_a = account.time_a;
+
+	if (time_a > 0) formula = "=R[-" + (h_ - 1) + "]C";
+	else formula = "=0";
+
+	sheet.getRange(1, col).setValue(account.name);
+	sheet.getRange(2 + h_*prev_time_a, col).setFormulaR1C1(formula);
+	sheet.getRange(2 + h_*time_a, col).setFormula("=" + account.balance.formatLocaleSignal());
+
+	if (spreadsheet.getSheetByName("Jan")) {
+		spreadsheet.getSheetByName("Jan")
+			.getRange(1, 6 + 5*c)
+			.setValue(account.name);
+	}
+}
+
+
+function refreshCashFlowReferences_() {
+	var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Cash Flow");
+	var formulas, db_accounts;
+	var string, mm, dd, i, k;
+
+	if (!sheet) return 1;
+
+	const h_ = TABLE_DIMENSION.height;
+
+	const ranges = [ "G", "L", "Q", "V", "AA" ];
+
+	const num_acc = getConstProperties_("number_accounts");
+	const yyyy = getConstProperties_("financial_year");
+
+	const db_accounts = getPropertiesService_("document", "json", "DB_TABLES");
+	db_accounts = db_accounts.accounts;
+
+	formulas = [ "=0 + B4" ];
+
+	for (i = 1; i < 12; i++) {
+		dd = new Date(yyyy, i, 0).getDate();
+		formulas[i] = "=" + rollA1Notation(3 + dd, 4*i - 1) + " + " + rollA1Notation(4, 2 + 4*i);
+	}
+
+	for (k = 0; k < num_acc; k++) {
+		mm = db_accounts.data[k].time_a;
+		formulas[mm] += " + \'_Backstage\'!" + ranges[k] + (2 + h_*mm);
+	}
+
+	for (i = 0; i < 12; i++) {
+		sheet.getRange(4, 3 + 4*i).setFormula(formulas[i]);
+	}
+}
