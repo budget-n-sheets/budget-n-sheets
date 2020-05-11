f7ae92c1e241d6ad450a220ff8dff510b572cf0e
Guilherme T Maeoka <gui.mspace@gmail.com>
2020-05-11 10:36:19 -0300

    Fix targeted user to get email

--- src/gas-common/libgas-common.js
+++ src/gas-common/libgas-common.js
@@ -43,7 +43,7 @@ function isReAuthorizationRequired_() {
 
 		htmlMessage = htmlTemplate.evaluate();
 		MailApp.sendEmail(
-			Session.getActiveUser().getEmail(),
+			Session.getEffectiveUser().getEmail(),
 			"Authorization Required",
 			htmlMessage.getContent(), {
 				name: "Add-on Budget n Sheets",
