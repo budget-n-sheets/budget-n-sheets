571d3aaed75b111ba4606574c263221843f54a3d
Guilherme T Maeoka <gui.mspace@gmail.com>
2020-06-22 19:53:51 -0300

    Set BSBLANK() during backstage setup

--- src/setup.js
+++ src/setup.js
@@ -726,7 +726,7 @@ function setupSettings_(yyyy_mm) {
 function setupBackstage_() {
 	console.time("add-on/setup/backstage");
 	var sheet = SPREADSHEET.getSheetByName("_Backstage");
-	var wallet, accounts, formula;
+	var wallet, accounts, cards, formula;
 	var income, expenses;
 	var n, i, k;
 
@@ -742,6 +742,7 @@ function setupBackstage_() {
 	const combo = [ "C5:D404", "H5:I404", "M5:N404", "R5:S404", "W5:X404", "AB5:AC404" ];
 	const balance1 = [ "G2", "L2", "Q2", "V2", "AA2", "G12", "L12", "Q12", "V12", "AA12", "G22", "L22", "Q22", "V22", "AA22", "G32", "L32", "Q32", "V32", "AA32", "G42", "L42", "Q42", "V42", "AA42", "G52", "L52", "Q52", "V52", "AA52", "G62", "L62", "Q62", "V62", "AA62", "G72", "L72", "Q72", "V72", "AA72", "G82", "L82", "Q82", "V82", "AA82", "G92", "L92", "Q92", "V92", "AA92", "G102", "L102", "Q102", "V102", "AA102", "G112", "L112", "Q112", "V112", "AA112" ];
 	const balance2 = [ "0", "0", "0", "0", "0", "G3", "L3", "Q3", "V3", "AA3", "G13", "L13", "Q13", "V13", "AA13", "G23", "L23", "Q23", "V23", "AA23", "G33", "L33", "Q33", "V33", "AA33", "G43", "L43", "Q43", "V43", "AA43", "G53", "L53", "Q53", "V53", "AA53", "G63", "L63", "Q63", "V63", "AA63", "G73", "L73", "Q73", "V73", "AA73", "G83", "L83", "Q83", "V83", "AA83", "G93", "L93", "Q93", "V93", "AA93", "G103", "L103", "Q103", "V103", "AA103" ];
+	const v_cards = [ "D6:D405", "J6:J405", "P6:P405", "V6:V405", "AB6:AB405", "AH6:AH405", "AN6:AN405", "AT6:AT405", "AZ6:AZ405", "BF6:BF405", "BL6:BL405", "BR6:BR405" ];
 
 	const width = w_*num_acc;
 	const height = 120;
@@ -750,13 +751,21 @@ function setupBackstage_() {
 
 	wallet = new Array(height);
 	accounts = new Array(height);
+	cards = new Array(height);
 
 	n = height;
 	while (n--) {
-		wallet[n] = [ null ];
+		wallet[n] = new Array(5);
 		accounts[n] = new Array(width);
+		cards[n] = [ null ];
 
 		i = -1;
+		while (++i < 5) {
+			wallet[n][i] = null;
+			accounts[n][i] = null;
+		}
+
+		i--;
 		while (++i < width) {
 			accounts[n][i] = null;
 		}
@@ -779,17 +788,22 @@ function setupBackstage_() {
 		income = "0";
 		expenses = "0";
 
+		wallet[h_*i][4] = "BSBLANK(TRANSPOSE(\'" + MN_SHORT[i] + "\'!" + values[k] + "))";
+
 		formula = "NOT(REGEXMATCH(\'" + MN_SHORT[i] + "\'!" + tags[k] + "; \"#ign\"))";
 		formula = "NOT(ISBLANK(\'" + MN_SHORT[i] + "\'!" + values[k] + ")); " + formula;
 		formula = "FILTER(\'" + MN_SHORT[i] + "\'!" + values[k] + "; " + formula + ")";
 		formula = "SUM(IFNA(" + formula + "; 0))";
 		wallet[2 + h_*i][0] = formula;
 
+		cards[h_*i][0] = "BSBLANK(TRANSPOSE(\'Cards\'!" + v_cards[i] + "))";
+
 		for (; k < num_acc; k++) {
 			income += " + " + rollA1Notation(6 + h_*i, 8 + w_*k);
 			expenses += " + " + rollA1Notation(4 + h_*i, 7 + w_*k);
 
 			accounts[h_*i][w_*k] = "=" + balance2[5*i + k];
+			accounts[h_*i][4 + w_*k] = "BSBLANK(TRANSPOSE(\'" + MN_SHORT[i] + "\'!" + values[1 + k] + "))";
 
 			formula = "NOT(ISBLANK(\'" + MN_SHORT[i] + "\'!" + values[1 + k] + "))";
 			formula = "FILTER(\'" + MN_SHORT[i] + "\'!" + values[1 + k] + "; " + formula + ")";
@@ -812,8 +826,9 @@ function setupBackstage_() {
 		wallet[3 + h_*i][0] = expenses;
 	}
 
-	sheet.getRange(2, 2, height, 1).setFormulas(wallet);
+	sheet.getRange(2, 2, height, 5).setFormulas(wallet);
 	sheet.getRange(2, 7, height, width).setFormulas(accounts);
+	sheet.getRange(2, 6 + w_*num_acc + w_, height, 1).setFormulas(cards);
 
 	console.timeEnd("add-on/setup/backstage");
 }
