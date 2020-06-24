3edd6bfb0372e14f1600e2a1cafebe48241c2f7f
Guilherme T Maeoka <gui.mspace@gmail.com>
2020-06-24 10:59:42 -0300

    Fix iteration counter

--- src/setup.js
+++ src/setup.js
@@ -572,20 +572,22 @@ function setupTags_() {
 
 function setupTables_() {
 	console.time("add-on/setup/tables");
-	var ids, acc, r, i, k;
+	var ids, acc, r, i, j, k;
 
 	const init_month = SETUP_SETTINGS["init_month"];
 	const list_acc = SETUP_SETTINGS["list_acc"];
 	const num_acc = SETUP_SETTINGS["number_accounts"];
 
 	i = 0;
+	j = 0;
 	ids = [ ];
-	while (i < 1 + num_acc && i < 99) {
+	while (j < 1 + num_acc && i < 99) {
 		r = randomString(7, "lonum");
 		if (ids.indexOf(r) === -1) {
-			ids[i] = r;
-			i++;
+			ids[j] = r;
+			j++;
 		}
+		i++;
 	}
 	if (ids.length < 1 + num_acc) throw new Error("Could not generate unique IDs.");
 
