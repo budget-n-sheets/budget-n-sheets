56544ecc61fc13d4d79cb65ac754836ded302113
Guilherme T Maeoka <gui.mspace@gmail.com>
2020-07-11 15:26:42 -0300

    Show selected card balance in simple view mode

--- src/tools/view_mode.js
+++ src/tools/view_mode.js
@@ -1,6 +1,7 @@
 function viewModeSimple() {
   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet()
   var sheet, i, k
+  var expr, head, cell
 
   const num_acc = getConstProperties_('number_accounts')
 
@@ -25,19 +26,39 @@ function viewModeSimple() {
 
     sheet.hideRows(2, 2)
   }
+  SpreadsheetApp.flush()
 
   sheet = spreadsheet.getSheetByName('Cards')
   if (!sheet) return
   if (sheet.getMaxRows() < 4) return
-  sheet.hideRows(2, 3)
 
+  const h_ = TABLE_DIMENSION.height;
+  const w_ = TABLE_DIMENSION.width;
+
+  const col = 2 + w_ + w_*num_acc;
+
+  for (i = 0; i < 12; i++) {
+    head = rollA1Notation(2, 1 + 6*i);
+    cell = "\'_Backstage\'!" + rollA1Notation(2 + h_*i, col);
+
+    expr = "OFFSET(" + cell + "; 4; 5*" + head + "; 1; 1)";
+    expr = "\"Balance: \"; TEXT(" + expr + "; \"#,##0.00;(#,##0.00)\")";
+
+    sheet.getRange(2, 4 + 6*i, 3, 2).breakApart()
+    sheet.getRange(2, 4 + 6*i, 1, 2)
+      .merge()
+      .setFormula('CONCATENATE(' + expr + ')')
+  }
+
+  sheet.hideRows(3, 2)
   SpreadsheetApp.flush()
 }
 
 function viewModeComplete() {
   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet()
   var sheet, i, k
-  var expr1, expr2, expr3, expr4
+  var formula, expr1, expr2, expr3, expr4
+  var head, cell
 
   const h_ = TABLE_DIMENSION.height;
   const w_ = TABLE_DIMENSION.width;
@@ -74,11 +95,31 @@ function viewModeComplete() {
       sheet.getRange(1, 6 + 5*k, 1, 2).setBorder(null, null, true, null, null, null, '#000000', SpreadsheetApp.BorderStyle.SOLID_MEDIUM)
     }
   }
+  SpreadsheetApp.flush()
 
   sheet = spreadsheet.getSheetByName('Cards')
   if (!sheet) return
   if (sheet.getMaxRows() < 4) return
-  sheet.showRows(2, 3)
 
+  const col = 2 + w_ + w_*num_acc;
+
+  sheet.showRows(3, 2)
+
+  for (i = 0; i < 12; i++) {
+    head = rollA1Notation(2, 1 + 6*i);
+    cell = "\'_Backstage\'!" + rollA1Notation(2 + h_*i, col);
+
+    expr1 = "OFFSET(" + cell + "; 1; 5*" + head + "; 1; 1)";
+    expr1 = "\"Credit: \"; TEXT(" + expr1 + "; \"#,##0.00;(#,##0.00)\"); \"\n\"; ";
+
+    expr2 = "OFFSET(" + cell + "; 3; 5*" + head + "; 1; 1)";
+    expr2 = "\"Expenses: \"; TEXT(" + expr2 + "; \"#,##0.00;(#,##0.00)\"); \"\n\"; ";
+
+    expr3 = "OFFSET(" + cell + "; 4; 5*" + head + "; 1; 1)";
+    expr3 = "\"Balance: \"; TEXT(" + expr3 + "; \"#,##0.00;(#,##0.00)\")";
+
+    formula = "CONCATENATE(" + expr1 + expr2 + "\"\n\"; " + expr3 + ")";
+    sheet.getRange(2, 4 + 6*i, 3, 2).merge().setFormula(formula);
+  }
   SpreadsheetApp.flush()
 }
