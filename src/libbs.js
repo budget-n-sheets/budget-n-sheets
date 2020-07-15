7d9b48b97f3c3d55106f00f1ddac810fdfee36ea
Guilherme T Maeoka <gui.mspace@gmail.com>
2020-07-15 15:48:05 -0300

    Apply semistandard coding style

--- src/libbs.js
+++ src/libbs.js
@@ -1,142 +1,137 @@
 /**
  * Number format $ x,xx0.00;-$ x,xx0.00
  */
-function numberFormatCurrency(p_dec_p) {
-	var DEC_P, DEC_PS;
+function numberFormatCurrency (p_dec_p) {
+  var DEC_P, DEC_PS;
 
-	if (p_dec_p != null) {
-		DEC_P = p_dec_p ? "." : ",";
-	} else {
-		DEC_P = getSpreadsheetSettings_("decimal_separator") ? "." : ",";
-	}
+  if (p_dec_p != null) {
+    DEC_P = p_dec_p ? '.' : ',';
+  } else {
+    DEC_P = getSpreadsheetSettings_('decimal_separator') ? '.' : ',';
+  }
 
-	DEC_PS = (DEC_P === "." ? "," : ".");
+  DEC_PS = (DEC_P === '.' ? ',' : '.');
 
-	var n = this;
-	var s = n < 0 ? '-$ ' : '$ ';
-	var i = parseInt(n = Math.abs(+n || 0).toFixed(2)) + '';
-	var j = (j = i.length) > 3 ? j % 3 : 0;
-	return s + (j ? i.substr(0, j) + DEC_PS : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + DEC_PS) + (2 ? DEC_P + Math.abs(n - i).toFixed(2).slice(2) : '');
+  var n = this;
+  var s = n < 0 ? '-$ ' : '$ ';
+  var i = parseInt(n = Math.abs(+n || 0).toFixed(2)) + '';
+  var j = i.length;
+  j = j > 3 ? j % 3 : 0;
+  return s + (j ? i.substr(0, j) + DEC_PS : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + DEC_PS) + DEC_P + Math.abs(n - i).toFixed(2).slice(2);
 }
 
 /**
  * Number format x,xx0.00;(x,xx0.00)
  */
-function numberFormatFinancial(p_dec_p) {
-	var DEC_P, DEC_PS;
+function numberFormatFinancial (p_dec_p) {
+  var DEC_P, DEC_PS;
 
-	if (p_dec_p != null) {
-		DEC_P = p_dec_p ? "." : ",";
-	} else {
-		DEC_P = getSpreadsheetSettings_("decimal_separator") ? "." : ",";
-	}
+  if (p_dec_p != null) {
+    DEC_P = p_dec_p ? '.' : ',';
+  } else {
+    DEC_P = getSpreadsheetSettings_('decimal_separator') ? '.' : ',';
+  }
 
-	DEC_PS = (DEC_P === "." ? "," : ".");
+  DEC_PS = (DEC_P === '.' ? ',' : '.');
 
-	var n = this;
-	var s = n < 0 ? true : false;
-	var i = parseInt(n = Math.abs(+n || 0).toFixed(2)) + '';
-	var j = (j = i.length) > 3 ? j % 3 : 0;
-	var a = (j ? i.substr(0, j) + DEC_PS : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + DEC_PS) + (2 ? DEC_P + Math.abs(n - i).toFixed(2).slice(2) : '');
+  var n = this;
+  var s = n < 0;
+  var i = parseInt(n = Math.abs(+n || 0).toFixed(2)) + '';
+  var j = i.length;
+  j = j > 3 ? j % 3 : 0;
+  var a = (j ? i.substr(0, j) + DEC_PS : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + DEC_PS) + DEC_P + Math.abs(n - i).toFixed(2).slice(2);
 
-	if (s) {
-		a = '(' + a + ')';
-	}
+  if (s) {
+    a = '(' + a + ')';
+  }
 
-	return a;
+  return a;
 }
 
 /**
  * Number format +0.00;-0.00
  */
-function numberFormatLocaleSignal(p_dec_p) {
-	var DEC_P, DEC_PS;
+function numberFormatLocaleSignal (p_dec_p) {
+  var DEC_P;
 
-	if (p_dec_p != null) {
-		DEC_P = p_dec_p ? "." : ",";
-	} else {
-		DEC_P = getSpreadsheetSettings_("decimal_separator") ? "." : ",";
-	}
+  if (p_dec_p != null) {
+    DEC_P = p_dec_p ? '.' : ',';
+  } else {
+    DEC_P = getSpreadsheetSettings_('decimal_separator') ? '.' : ',';
+  }
 
-	DEC_PS = (DEC_P === "." ? "," : ".");
-
-	var n = this;
-	var s = n < 0 ? '-' : '+';
-	var i = parseInt(n = Math.abs(n).toFixed(2)) + '';
-	var j = i.length;
-	return s + i.substr(0, j) + DEC_P + Math.abs(n - i).toFixed(2).slice(2);
+  var n = this;
+  var s = n < 0 ? '-' : '+';
+  var i = parseInt(n = Math.abs(n).toFixed(2)) + '';
+  var j = i.length;
+  return s + i.substr(0, j) + DEC_P + Math.abs(n - i).toFixed(2).slice(2);
 }
 
 /**
  * Number format $0.00;-$0.00
  */
-function numberFormatCalendarSignal(p_dec_p) {
-	var DEC_P, DEC_PS;
+function numberFormatCalendarSignal (p_dec_p) {
+  var DEC_P;
 
-	if (p_dec_p != null) {
-		DEC_P = p_dec_p ? "." : ",";
-	} else {
-		DEC_P = getSpreadsheetSettings_("decimal_separator") ? "." : ",";
-	}
+  if (p_dec_p != null) {
+    DEC_P = p_dec_p ? '.' : ',';
+  } else {
+    DEC_P = getSpreadsheetSettings_('decimal_separator') ? '.' : ',';
+  }
 
-	DEC_PS = (DEC_P === "." ? "," : ".");
-
-	var n = this;
-	var s = n < 0 ? '-$' : '$';
-	var i = parseInt(n = Math.abs(n).toFixed(2)) + '';
-	var j = i.length;
-	return s + i.substr(0, j) + DEC_P + Math.abs(n - i).toFixed(2).slice(2);
+  var n = this;
+  var s = n < 0 ? '-$' : '$';
+  var i = parseInt(n = Math.abs(n).toFixed(2)) + '';
+  var j = i.length;
+  return s + i.substr(0, j) + DEC_P + Math.abs(n - i).toFixed(2).slice(2);
 }
 
-
-function getSpreadsheetDate() {
-	var timezone = SpreadsheetApp.getActiveSpreadsheet().getSpreadsheetTimeZone();
-	if (typeof timezone != "string" || timezone == "") {
-		timezone = "GMT";
-	}
-	var date = Utilities.formatDate(this, timezone, "yyyy-MM-dd'T'HH:mm:ss'Z'");
-	return new Date(date);
+function getSpreadsheetDate () {
+  var timezone = SpreadsheetApp.getActiveSpreadsheet().getSpreadsheetTimeZone();
+  if (typeof timezone !== 'string' || timezone === '') {
+    timezone = 'GMT';
+  }
+  var date = Utilities.formatDate(this, timezone, "yyyy-MM-dd'T'HH:mm:ss'Z'");
+  return new Date(date);
 }
 
+function getMonthDelta (mm) {
+  if (mm == null) {
+    mm = getSpreadsheetDate.call(DATE_NOW).getMonth();
+  }
 
-function getMonthDelta(mm) {
-	if (mm == null) {
-		mm = getSpreadsheetDate.call(DATE_NOW).getMonth();
-	}
+  switch (mm) {
+    case 0:
+      return [0, 3];
+    case 11:
+      return [-3, 0];
 
-	switch (mm) {
-		case 0:
-			return [ 0, 3 ];
-		case 11:
-			return [ -3, 0 ];
-
-		default:
-			return [ -2, 1 ];
-	}
+    default:
+      return [-2, 1];
+  }
 }
 
+function consoleLog_ (type, message, error) {
+  const parts = {};
 
-function consoleLog_(type, message, error) {
-	const parts = {};
+  for (var i in error) {
+    parts[i] = error[i];
+  }
 
-	for (var i in error) {
-		parts[i] = error[i];
-	}
+  const payload = {
+    message: message,
+    error: error,
+    parts: parts
+  };
 
-	const payload = {
-		message: message,
-		error: error,
-		parts: parts
-	};
+  switch (type) {
+    case 'warn':
+      console.warn(payload);
+      break;
 
-	switch (type) {
-		case 'warn':
-			console.warn(payload);
-			break;
-
-		default:
-		case 'error':
-			console.error(payload);
-			break;
-	}
+    default:
+    case 'error':
+      console.error(payload);
+      break;
+  }
 }
