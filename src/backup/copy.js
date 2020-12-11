71d5071408239ac6401f41e460af37481cc75567
Guilherme T Maeoka <gui.mspace@gmail.com>
2020-12-10 21:25:16 -0300

    Retrieve more details from origin spreadsheet

--- src/backup/copy.js
+++ src/backup/copy.js
@@ -52,3 +52,59 @@ function validateSpreadsheet (fileId) {
     last_updated: file.getLastUpdated().toString()
   };
 }
+
+function readSpreadsheetInfo (fileId) {
+  const spreadsheet = SpreadsheetApp.openById(fileId);
+  let sheet, values, cols;
+  let list;
+
+  const w_ = TABLE_DIMENSION.width;
+
+  const info = {
+    spreadsheet_title: spreadsheet.getName(),
+
+    financial_year: DATE_NOW.getFullYear(),
+    initial_month: DATE_NOW.getMonth(),
+
+    accounts: '',
+    cards: '',
+    tags: '-'
+  };
+
+  sheet = spreadsheet.getSheetByName('_Settings');
+  if (!sheet) return 1;
+
+  values = sheet.getRange(2, 2, 7, 1).getValues();
+
+  info.financial_year = values[0][0];
+  info.initial_month = MN_FULL[values[2][0]];
+  if (values[5][0] > 0) info.tags = 'Up to ' + values[5][0] + ' tag(s) found.';
+
+  sheet = spreadsheet.getSheetByName('_Backstage');
+  if (!sheet) return 1;
+
+  cols = sheet.getMaxColumns();
+  values = sheet.getRange(1, 2, 1, cols - 1).getValues();
+
+  cols = cols - 1 - 12 * w_;
+  const num_accs = (cols - (cols % w_)) / w_;
+  if (cols === 0) return 1;
+
+  list = [];
+  for (let i = 0; i < num_accs; i++) {
+    list[i] = values[0][w_ + w_ * i];
+  }
+  info.accounts = list.join(', ');
+
+  list = [];
+  cols = 2 * w_ + num_accs * w_;
+  for (let i = 0; i < 10; i++) {
+    if (values[0][cols + w_ * i] != '') {
+      let match = values[0][cols + w_ * i].match(/\w+/);
+      if (match) list.push(match);
+    }
+  }
+  info.cards = list.join(', ');
+
+  return info;
+}
