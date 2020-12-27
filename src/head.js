bc1986b573ef4b0437397aaf2070719c71758320
Guilherme T Maeoka <gui.mspace@gmail.com>
2020-12-27 17:21:51 -0300

    Add global SPREADSHEET_SPECS

--- src/head.js
+++ src/head.js
@@ -11,6 +11,39 @@ const CACHE_KEYS = ['class_version2', 'admin_settings', 'user_settings', 'spread
 
 const TABLE_DIMENSION = Object.freeze({ height: 10, width: 5 });
 
+const SPREADSHEET_SPECS = Object.freeze({
+  initial_height: 400,
+
+  ttt: {
+    header: ['day', 'transaction', 'value', 'tags'],
+    width: 4,
+    row: 5
+  },
+
+  cards: {
+    header: ['day', 'transaction', 'card', 'value', 'tags'],
+    width: 5,
+    row: 6
+  },
+
+  cash_flow: {
+    header: ['flow', 'balance', 'transactions'],
+    width: 3,
+    row: 4
+  },
+
+  tags: {
+    header: ['name',	'category',	'description',	'analytics',	'code']
+  },
+
+  backstage: {
+    square: {
+      height: 10,
+      width: 5
+    }
+  }
+});
+
 const CONSTANTS = Object.freeze({
   month_name: {
     short: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
