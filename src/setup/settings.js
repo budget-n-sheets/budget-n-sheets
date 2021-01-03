e03a52bb09553e6711b52408cc152a52d5f0408c
Guilherme T Maeoka <gui.mspace@gmail.com>
2021-01-03 19:12:47 -0300

    Remove unnecessary '=' symbol

--- src/setup/settings.js
+++ src/setup/settings.js
@@ -24,13 +24,13 @@ function setupSettings_ (yyyy_mm) {
   SETUP_SETTINGS.decimal_separator = dec_p;
 
   cell = [
-    ['=' + Number(SETUP_SETTINGS.financial_year)],
+    [Number(SETUP_SETTINGS.financial_year)],
     [buildFormulas.actual_month()],
-    ['=' + Number(SETUP_SETTINGS.init_month + 1)],
+    [Number(SETUP_SETTINGS.init_month + 1)],
     [buildFormulas.active_months()],
     [buildFormulas.m_factor()],
     [buildFormulas.count_tags()],
-    ['=RAND()'],
+    ['RAND()'],
     [SETUP_SETTINGS.decimal_places],
     [SETUP_SETTINGS.decimal_separator],
     ['CONCATENATE("#,##0."; REPT("0"; B9); ";(#,##0."; REPT("0"; B9); ")")']
