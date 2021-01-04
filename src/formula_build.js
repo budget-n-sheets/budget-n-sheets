376ac93b8e8def579945e81ea2a490f1bd67cab1
Guilherme T Maeoka <gui.mspace@gmail.com>
2021-01-04 11:42:17 -0300

    Fix definition of 'number_format'

--- src/formula_build.js
+++ src/formula_build.js
@@ -531,6 +531,16 @@ const FormulaBuild = Object.freeze({
     },
 
     Header: {
+      loadSettings: function (name) {
+        if (this._settings[name]) return;
+
+        switch (name) {
+          case 'decimal_places':
+            this._settings.decimal_places = getSpreadsheetSettings_('decimal_places');
+            break;
+        }
+      },
+
       balance: function (index, mm) {
         const balance = rollA1Notation(3 + TABLE_DIMENSION.height * mm, 7 + TABLE_DIMENSION.width * index);
 
@@ -544,21 +554,26 @@ const FormulaBuild = Object.freeze({
       },
 
       report: function (index, mm) {
+        this.loadSettings('decimal_places');
+
         const _h = TABLE_DIMENSION.height;
         const _w = TABLE_DIMENSION.width;
 
+        const dec_m = (this._settings.decimal_places > 0 ? '.' + '0'.repeat(this._settings.decimal_places) : '');
+        const number_format = '#,##0' + dec_m + ';' + '(#,##0' + dec_m + ')';
+
         let formula, part_1, part_2, part_3, part_4;
 
-        part_1 = 'TEXT(_Backstage!' + rollA1Notation(2 + _h * mm, 8 + _w * index) + '; "' + SETUP_SETTINGS.number_format + '")';
+        part_1 = 'TEXT(_Backstage!' + rollA1Notation(2 + _h * mm, 8 + _w * index) + '; "' + number_format + '")';
         part_1 = '"Withdrawal: ["; _Backstage!' + rollA1Notation(2 + _h * mm, 9 + _w * index) + '; "] "; ' + part_1 + '; "\n"; ';
 
-        part_2 = 'TEXT(_Backstage!' + rollA1Notation(3 + _h * mm, 8 + _w * index) + '; "' + SETUP_SETTINGS.number_format + '")';
+        part_2 = 'TEXT(_Backstage!' + rollA1Notation(3 + _h * mm, 8 + _w * index) + '; "' + number_format + '")';
         part_2 = '"Deposit: ["; _Backstage!' + rollA1Notation(3 + _h * mm, 9 + _w * index) + '; "] "; ' + part_2 + '; "\n"; ';
 
-        part_3 = 'TEXT(_Backstage!' + rollA1Notation(4 + _h * mm, 8 + _w * index) + '; "' + SETUP_SETTINGS.number_format + '")';
+        part_3 = 'TEXT(_Backstage!' + rollA1Notation(4 + _h * mm, 8 + _w * index) + '; "' + number_format + '")';
         part_3 = '"Trf. in: ["; _Backstage!' + rollA1Notation(4 + _h * mm, 9 + _w * index) + '; "] "; ' + part_3 + '; "\n"; ';
 
-        part_4 = 'TEXT(_Backstage!' + rollA1Notation(5 + _h * mm, 8 + _w * index) + '; "' + SETUP_SETTINGS.number_format + '")';
+        part_4 = 'TEXT(_Backstage!' + rollA1Notation(5 + _h * mm, 8 + _w * index) + '; "' + number_format + '")';
         part_4 = '"Trf. out: ["; _Backstage!' + rollA1Notation(5 + _h * mm, 9 + _w * index) + '; "] "; ' + part_4;
 
         formula = 'CONCATENATE(' + part_1 + part_2 + part_3 + part_4 + ')';
