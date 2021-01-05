7555d521aadc590d5a88ff206fe75f1bbc5b193b
Guilherme T Maeoka <gui.mspace@gmail.com>
2021-01-05 15:41:17 -0300

    Format in multiple-lines object key-value

--- src/quickstart/calendar.js
+++ src/quickstart/calendar.js
@@ -1,15 +1,45 @@
 const QUICKSTART_DATA_CALENDAR = Object.freeze({
   1: [
-    { day: 2, title: 'The simplest event', description: 'acc_name\nvalue\n---\nThis simple event has the name of an account and a number formatted.', value: -1.23 },
-    { day: 3, title: 'Muted event', description: 'acc_name\nvalue\n\n@muted\n---\nThis event has the "@muted" indicator, so it is not included in cash flow, nor posted in the table.', value: -1.23 },
-    { day: 5, title: 'Income', description: 'acc_name\nvalue\n\n#trf #rct\n---\nSimilar to <b>The simplest event</b> but with a few tags. While the tags don\'t play any role in cash flow, they are posted in the table along with the other details.', value: 1234.56 }
+    {
+      day: 2,
+      title: 'The simplest event',
+      description: 'acc_name\nvalue\n---\nThis simple event has the name of an account and a number formatted.',
+      value: -1.23
+    },
+    {
+      day: 3,
+      title: 'Muted event',
+      description: 'acc_name\nvalue\n\n@muted\n---\nThis event has the "@muted" indicator, so it is not included in cash flow, nor posted in the table.',
+      value: -1.23
+    },
+    {
+      day: 5,
+      title: 'Income',
+      description: 'acc_name\nvalue\n\n#trf #rct\n---\nSimilar to <b>The simplest event</b> but with a few tags. While the tags don\'t play any role in cash flow, they are posted in the table along with the other details.',
+      value: 1234.56
+    }
   ],
   2: [
-    { day: 2, title: 'The simplest card event', description: 'card_code\nvalue\n---\nThis simple event has the code of a card and a number formatted. This event is not synced with the cash flow.', value: -1.23 },
-    { day: 7, title: 'Card bill payment', description: 'card_code\n\n#qcc\n---\nThis event has the "#qcc" built-in tag and no number formatted. The add-on gets the card\'s balance of the previous month and puts it in the cash flow.' }
+    {
+      day: 2,
+      title: 'The simplest card event',
+      description: 'card_code\nvalue\n---\nThis simple event has the code of a card and a number formatted. This event is not synced with the cash flow.',
+      value: -1.23
+    },
+    {
+      day: 7,
+      title: 'Card bill payment',
+      description: 'card_code\n\n#qcc\n---\nThis event has the "#qcc" built-in tag and no number formatted. The add-on gets the card\'s balance of the previous month and puts it in the cash flow.'
+    }
   ],
   3: [
-    { day: 11, length: 2, title: 'Two-days event', description: 'acc_name\nvalue\n---\nMultiple-days events behave like a one-day event.', value: -1.23 }
+    {
+      day: 11,
+      length: 2,
+      title: 'Two-days event',
+      description: 'acc_name\nvalue\n---\nMultiple-days events behave like a one-day event.',
+      value: -1.23
+    }
   ]
 });
 
