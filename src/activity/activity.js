bc061c965dd66b63fd71cc8c6406d3e5de44b5f0
Guilherme T Maeoka <gui.mspace@gmail.com>
2021-01-11 17:43:06 -0300

    Move month calculation to toolResumeActivity_()

--- src/activity/activity.js
+++ src/activity/activity.js
@@ -4,21 +4,27 @@ function toolResumeActivity_ () {
   if (!isInstalled_()) return;
   if (onlineUpdate_()) return;
 
-  const r = switchActivity_(select);
+  const name = SpreadsheetApp.getActiveSheet().getSheetName();
+  const mm = MONTH_NAME.short.indexOf(name);
+
+  if (mm === -1) {
+    SpreadsheetApp.getUi().alert(
+      "Can't change activity",
+      'Select a month to resume the activity.',
+      SpreadsheetApp.getUi().ButtonSet.OK);
+    return;
+  }
+
+  const r = switchActivity_(select, mm);
 
   if (r === 1) {
     SpreadsheetApp.getActiveSheet().toast(
       'The add-on is busy. Try again in a moment.',
       'Budget n Sheets');
-  } else if (r === 2) {
-    SpreadsheetApp.getUi().alert(
-      "Can't change activity",
-      'Select a month to change the activity.',
-      SpreadsheetApp.getUi().ButtonSet.OK);
   }
 }
 
-function switchActivity_ (select) {
+function switchActivity_ (select, param) {
   const lock = LockService.getDocumentLock();
   try {
     lock.waitLock(200);
@@ -47,10 +53,7 @@ function switchActivity_ (select) {
 
     suspendActivity_(0, mm);
   } else if (select === 'resume') {
-    mm = MONTH_NAME.short.indexOf(SpreadsheetApp.getActiveSheet().getSheetName());
-    if (mm === -1) return 2;
-
-    resumeActivity_(mm);
+    resumeActivity_(param);
   }
 
   lock.releaseLock();
