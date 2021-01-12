edfdd4817e44048b7574ffda59f2a1adaeb1ccdf
Guilherme T Maeoka <gui.mspace@gmail.com>
2021-01-12 06:40:57 -0300

    Rename variables for better context

--- src/activity/resume.js
+++ src/activity/resume.js
@@ -2,7 +2,7 @@ function resumeActivity_ (mm) {
   const formulasBuild = FormulaBuild.backstage();
 
   const spreadsheet = SpreadsheetApp2.getActiveSpreadsheet();
-  const sheet = spreadsheet.getSheetByName('_Backstage');
+  const sheetBackstage = spreadsheet.getSheetByName('_Backstage');
   let range1A1, range2A1, formula, width, i, k;
   let income, expenses;
 
@@ -15,7 +15,7 @@ function resumeActivity_ (mm) {
   const balance1 = ['G2', 'L2', 'Q2', 'V2', 'AA2', 'G12', 'L12', 'Q12', 'V12', 'AA12', 'G22', 'L22', 'Q22', 'V22', 'AA22', 'G32', 'L32', 'Q32', 'V32', 'AA32', 'G42', 'L42', 'Q42', 'V42', 'AA42', 'G52', 'L52', 'Q52', 'V52', 'AA52', 'G62', 'L62', 'Q62', 'V62', 'AA62', 'G72', 'L72', 'Q72', 'V72', 'AA72', 'G82', 'L82', 'Q82', 'V82', 'AA82', 'G92', 'L92', 'Q92', 'V92', 'AA92', 'G102', 'L102', 'Q102', 'V102', 'AA102', 'G112', 'L112', 'Q112', 'V112', 'AA112'];
   const balance2 = ['0', '0', '0', '0', '0', 'G3', 'L3', 'Q3', 'V3', 'AA3', 'G13', 'L13', 'Q13', 'V13', 'AA13', 'G23', 'L23', 'Q23', 'V23', 'AA23', 'G33', 'L33', 'Q33', 'V33', 'AA33', 'G43', 'L43', 'Q43', 'V43', 'AA43', 'G53', 'L53', 'Q53', 'V53', 'AA53', 'G63', 'L63', 'Q63', 'V63', 'AA63', 'G73', 'L73', 'Q73', 'V73', 'AA73', 'G83', 'L83', 'Q83', 'V83', 'AA83', 'G93', 'L93', 'Q93', 'V93', 'AA93', 'G103', 'L103', 'Q103', 'V103', 'AA103'];
 
-  if (!sheet) return;
+  if (!sheetBackstage) return;
   if (!spreadsheet.getSheetByName('Cards')) return;
   if (!spreadsheet.getSheetByName(MONTH_NAME.short[mm])) return;
 
@@ -36,32 +36,32 @@ function resumeActivity_ (mm) {
   }
 
   formula = formulasBuild.wallet().bsblank(mm, values[0]);
-  sheet.getRange(2 + h_ * mm, 6).setFormula(formula);
+  sheetBackstage.getRange(2 + h_ * mm, 6).setFormula(formula);
 
   formula = formulasBuild.wallet().expenses_ign(max1 - 4, mm, rollA1Notation(2 + h_ * mm, 6));
-  sheet.getRange(4 + h_ * mm, 2).setFormula(formula);
+  sheetBackstage.getRange(4 + h_ * mm, 2).setFormula(formula);
 
   income = '0';
   expenses = '0';
 
   const formulasAcc = formulasBuild.accounts();
   for (k = 0; k < num_acc; k++) {
-    range1A1 = rollA1Notation(2 + h_ * mm, 11 + w_ * k);
+    const bsblank = rollA1Notation(2 + h_ * mm, 11 + w_ * k);
 
     income += ' + ' + rollA1Notation(6 + h_ * mm, 8 + w_ * k);
     expenses += ' + ' + rollA1Notation(4 + h_ * mm, 7 + w_ * k);
 
     accounts[0][w_ * k] = '=' + balance2[5 * mm + k];
-    accounts[0][1 + w_ * k] = formulasAcc.bsreport(mm, tags[1 + k], combo[1 + k], range1A1);
+    accounts[0][1 + w_ * k] = formulasAcc.bsreport(mm, tags[1 + k], combo[1 + k], bsblank);
     accounts[0][4 + w_ * k] = formulasAcc.bsblank(mm, values[1 + k]);
-    accounts[1][w_ * k] = formulasAcc.balance(mm, values[1 + k], balance1[5 * mm + k], range1A1);
-    accounts[2][w_ * k] = formulasAcc.expenses_ign(mm, values[1 + k], tags[1 + k], range1A1);
+    accounts[1][w_ * k] = formulasAcc.balance(mm, values[1 + k], balance1[5 * mm + k], bsblank);
+    accounts[2][w_ * k] = formulasAcc.expenses_ign(mm, values[1 + k], tags[1 + k], bsblank);
   }
 
-  sheet.getRange(3 + h_ * mm, 2).setFormula(income);
-  sheet.getRange(5 + h_ * mm, 2).setFormula(expenses);
-  sheet.getRange(2 + h_ * mm, 7, h_, width).setFormulas(accounts);
-  sheet.getRangeList([
+  sheetBackstage.getRange(3 + h_ * mm, 2).setFormula(income);
+  sheetBackstage.getRange(5 + h_ * mm, 2).setFormula(expenses);
+  sheetBackstage.getRange(2 + h_ * mm, 7, h_, width).setFormulas(accounts);
+  sheetBackstage.getRangeList([
     rollA1Notation(6 + h_ * mm, 2),
     rollA1Notation(7 + h_ * mm, 2)
   ]).setFormulaR1C1('R[-2]C[' + (col - w_ - 2) + ']');
@@ -75,14 +75,14 @@ function resumeActivity_ (mm) {
     for (i = 1 + mm; i < actual_month; i++) {
       rangeList[i - 1 - mm] = rollA1Notation(2 + h_ * i, 2 + w_ + w_ * k);
 
-      range1A1 = rollA1Notation(2 + h_ * i, 11 + w_ * k);
+      const bsblank = rollA1Notation(2 + h_ * i, 11 + w_ * k);
 
-      formula = formulasAcc.balance(i, values[1 + k], balance1[5 * i + k], range1A1);
-      sheet.getRange(3 + h_ * i, 2 + w_ + w_ * k).setFormula(formula);
+      formula = formulasAcc.balance(i, values[1 + k], balance1[5 * i + k], bsblank);
+      sheetBackstage.getRange(3 + h_ * i, 2 + w_ + w_ * k).setFormula(formula);
     }
 
     if (rangeList.length > 0) {
-      sheet.getRangeList(rangeList).setFormulaR1C1('R[-' + (h_ - 1) + ']C');
+      sheetBackstage.getRangeList(rangeList).setFormulaR1C1('R[-' + (h_ - 1) + ']C');
     }
   }
 
@@ -94,7 +94,7 @@ function resumeActivity_ (mm) {
     if (account.time_a < mm) continue;
 
     formula = '=' + FormatNumber.localeSignal(account.balance);
-    sheet.getRange(2 + h_ * account.time_a, 2 + w_ + w_ * k).setFormula(formula);
+    sheetBackstage.getRange(2 + h_ * account.time_a, 2 + w_ + w_ * k).setFormula(formula);
   }
 
   const list1 = [];
@@ -116,24 +116,22 @@ function resumeActivity_ (mm) {
   for (k = 2; k <= 10; k++) {
     formula += ' + RC[' + w_ * k + ']';
   }
-  sheet.getRange(3 + h_ * mm, col - w_, 4, 1).setFormulaR1C1(formula);
+  sheetBackstage.getRange(3 + h_ * mm, col - w_, 4, 1).setFormulaR1C1(formula);
 
   formula = formulasCards.bsblank(max2, mm);
-  sheet.getRange(2 + h_ * mm, 4 + col - w_).setFormula(formula);
-
-  let header1, header2;
+  sheetBackstage.getRange(2 + h_ * mm, 4 + col - w_).setFormula(formula);
 
   for (k = 0; k < 10; k++) {
     range1A1 = rollA1Notation(6, 4 + 6 * mm, max2);
     range2A1 = rollA1Notation(6, 3 + 6 * mm, max2);
 
-    header1 = rollA1Notation(1, col + w_ * k);
-    header2 = rollA1Notation(2 + h_ * mm, 4 + col + w_ * k);
+    const regex = rollA1Notation(1, col + w_ * k);
+    const bsblank = rollA1Notation(2 + h_ * mm, 4 + col + w_ * k);
 
-    cards[1][w_ * k] = formulasCards.credit(max2, mm, header1, header2);
-    cards[2][w_ * k] = formulasCards.expenses_ign(max2, mm, header1, header2);
-    cards[3][w_ * k] = formulasCards.expenses(max2, mm, header1, header2);
-    cards[3][1 + w_ * k] = formulasCards.bscardpart(max2, mm, rollA1Notation(1, col + w_ * k), header2);
+    cards[1][w_ * k] = formulasCards.credit(max2, mm, regex, bsblank);
+    cards[2][w_ * k] = formulasCards.expenses_ign(max2, mm, regex, bsblank);
+    cards[3][w_ * k] = formulasCards.expenses(max2, mm, regex, bsblank);
+    cards[3][1 + w_ * k] = formulasCards.bscardpart(max2, mm, rollA1Notation(1, col + w_ * k), bsblank);
 
     list1[k] = rollA1Notation(6 + h_ * mm, col + w_ * k);
     list2[k] = rollA1Notation(6 + h_ * mm, 1 + col + w_ * k);
@@ -141,18 +139,18 @@ function resumeActivity_ (mm) {
     list4[k] = rollA1Notation(2 + h_ * mm, 4 + col + w_ * k);
   }
 
-  sheet.getRange(2 + h_ * mm, col, h_, 10 * w_).setFormulas(cards);
+  sheetBackstage.getRange(2 + h_ * mm, col, h_, 10 * w_).setFormulas(cards);
 
-  sheet.getRangeList(list1).setFormulaR1C1('R[-1]C + R[-3]C');
-  sheet.getRangeList(list2).setFormulaR1C1('R[-1]C + R[-4]C + RC[-1]');
-  sheet.getRangeList(list3).setFormulaR1C1('MIN(R[-1]C; R[-1]C - R[3]C)');
-  sheet.getRangeList(list4).setFormula(rollA1Notation(2 + h_ * mm, 4 + col - w_));
+  sheetBackstage.getRangeList(list1).setFormulaR1C1('R[-1]C + R[-3]C');
+  sheetBackstage.getRangeList(list2).setFormulaR1C1('R[-1]C + R[-4]C + RC[-1]');
+  sheetBackstage.getRangeList(list3).setFormulaR1C1('MIN(R[-1]C; R[-1]C - R[3]C)');
+  sheetBackstage.getRangeList(list4).setFormula(rollA1Notation(2 + h_ * mm, 4 + col - w_));
   SpreadsheetApp.flush();
 
   const db_cards = getDbTables_('cards');
   for (k = 0; k < db_cards.count; k++) {
     formula = '=' + FormatNumber.localeSignal(db_cards.data[k].limit);
-    sheet.getRange(2 + h_ * mm, 1 + col + w_ * k).setFormula(formula);
+    sheetBackstage.getRange(2 + h_ * mm, 1 + col + w_ * k).setFormula(formula);
   }
 
   let status = getSpreadsheetSettings_('optimize_load');
