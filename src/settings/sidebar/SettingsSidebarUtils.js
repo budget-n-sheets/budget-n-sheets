b2e7e961ac5262451ab736382c76eec825e52477
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-07-21 14:50:55 -0300

    Fix coding style issues

--- src/settings/sidebar/SettingsSidebarUtils.js
+++ src/settings/sidebar/SettingsSidebarUtils.js
@@ -12,42 +12,42 @@ class SettingsSidebarUtils {
   }
 
   panelSettings_ () {
-    const _values = {};
+    const values = {};
 
     const spreadsheet = SpreadsheetApp2.getActiveSpreadsheet();
     const financial_year = getConstProperties_('financial_year');
     const isOperationActive = (financial_year >= DATE_NOW.getFullYear());
 
-    _values.isOperationActive = isOperationActive;
+    values.isOperationActive = isOperationActive;
 
     if (isOperationActive) {
       const calendars = getAllOwnedCalendars();
-      _values.isCalendarEnabled = (calendars.md5.length > 0);
-      _values.calendars_data = calendars;
+      values.isCalendarEnabled = (calendars.md5.length > 0);
+      values.calendars_data = calendars;
     } else {
-      _values.isCalendarEnabled = false;
+      values.isCalendarEnabled = false;
     }
 
-    _values.doc_name = spreadsheet.getName();
-    _values.financial_year = financial_year;
+    values.doc_name = spreadsheet.getName();
+    values.financial_year = financial_year;
 
-    return _values;
+    return values;
   }
 
   panelMaintenance_ () {
-    const _values = {};
+    const values = {};
 
     const spreadsheet = SpreadsheetApp2.getActiveSpreadsheet();
     const owner = spreadsheet.getOwner();
 
     if (owner) {
-      _values.isOwner = (Session.getEffectiveUser().getEmail() === owner.getEmail());
-      _values.isSharedDrive = false;
+      values.isOwner = (Session.getEffectiveUser().getEmail() === owner.getEmail());
+      values.isSharedDrive = false;
     } else {
-      _values.isOwner = false;
-      _values.isSharedDrive = true;
+      values.isOwner = false;
+      values.isSharedDrive = true;
     }
 
-    return _values;
+    return values;
   }
 }
