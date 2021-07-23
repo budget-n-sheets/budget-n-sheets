8df425ff295948556d7fb9399801a0af72be18b5
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-07-23 14:40:00 -0300

    RapidAccess: add channel for Cache service

--- src/RapidAccess.js
+++ src/RapidAccess.js
@@ -1,10 +1,35 @@
 const Goldfish = {
-  services: {}
+  services: {
+    cache: {
+      document: null,
+      user: null
+    }
+  }
 };
+
+class RapidAccessServicesCache {
+  constructor (cache) {
+    this._cache = cache;
+  }
+
+  document () {
+    return this._cache.document ||
+          (this._cache.document = CacheService.getDocumentCache());
+  }
+
+  user () {
+    return this._cache.user ||
+          (this._cache.user = CacheService.getUserCache());
+  }
+}
 
 class RapidAccessServices {
   constructor (services) {
     this._services = services;
+  }
+
+  cache () {
+    return new RapidAccessServicesCache(this._services.cache);
   }
 }
 
