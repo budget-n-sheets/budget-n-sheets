d632e0af8b015ca7a8d44d778efd884f7180c12d
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-07-28 15:23:38 -0300

    Bump add-on version to 0.42.3

--- src/update.js
+++ src/update.js
@@ -7,7 +7,7 @@ const PATCH_THIS = Object.freeze({
       [], [], [], [], [], [], [], [], [], [],
       [update_v0m40p0_, update_v0m40p1_],
       [null, null, null, update_v0m41p3_, null],
-      [null, null, null]
+      [null, null, null, null]
     ]
   ],
   beta_list: []
