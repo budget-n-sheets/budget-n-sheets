a3ef4a58e91747f5f465819046bacfaf4a3ce9e5
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-08-01 15:51:08 -0300

    Add method update()

--- src/TablesService/CardsService.js
+++ src/TablesService/CardsService.js
@@ -16,4 +16,37 @@ class CardsService extends TablesService {
   hasSlotAvailable () {
     return this._db.count < 10;
   }
+
+  update (metadata) {
+    if (!this.hasId(metadata.id)) return 1;
+
+    metadata.code = metadata.code.trim();
+    if (!/^\w+$/.test(metadata.code)) return 10;
+
+    const pos = this._db.ids.indexOf(metadata.id);
+    for (let i = 0; i < this._db.codes.length; i++) {
+      if (i !== pos && this._db.codes[i] === metadata.code) return 11;
+    }
+
+    let aliases = metadata.aliases.match(/\w+/g);
+    if (aliases == null) aliases = [];
+
+    let c = aliases.indexOf(metadata.code);
+    while (c !== -1) {
+      aliases.splice(c, 1);
+      c = aliases.indexOf(metadata.code);
+    }
+
+    this._db.codes[pos] = metadata.code;
+
+    this._db.data[pos] = {
+      id: metadata.id,
+      name: metadata.name,
+      code: metadata.code,
+      aliases: aliases,
+      limit: Number(metadata.limit)
+    };
+
+    return this;
+  }
 }
