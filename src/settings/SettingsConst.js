5c29b6ea3fa0075f214e839a2c852dc0476d5b6a
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-08-02 14:24:17 -0300

    Remove method load_()

--- src/settings/SettingsConst.js
+++ src/settings/SettingsConst.js
@@ -1,14 +1,10 @@
 class SettingsConst {
-  static load_ () {
-    this._properties = RapidAccess.properties().const();
-  }
-
   static getValueOf (key) {
     switch (key) {
       case 'financial_year':
       case 'number_accounts':
       case 'date_created':
-        this.load_();
+        this._properties = RapidAccess.properties().const();
         return this._properties[key];
 
       default:
