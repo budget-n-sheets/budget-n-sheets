7e8c7874c370ce16328d266f0765664ce25534e0
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-08-03 18:48:43 -0300

    Draft class SpreadsheetService

---
+++ src/gas-common/SpreadsheetService.js
@@ -0,0 +1,37 @@
+class SpreadsheetService {
+  static copySheetsFromSource () {
+    const source = SpreadsheetApp.openById(APPS_SCRIPT_GLOBAL.template_id);
+    const destination = Spreadsheet2.getActiveSpreadsheet();
+    const sheets = destination.getSheets();
+
+    const list = APPS_SCRIPT_GLOBAL.template_sheets;
+    list.forEach(name => {
+      source.getSheetByName(name)
+        .copyTo(destination)
+        .setName(name);
+    });
+
+    sheets.forEach(sheet => destination.deleteSheet(sheet));
+  }
+
+  static deleteAllSheets () {
+    const spreadsheet = Spreadsheet2.getActiveSpreadsheet();
+    const sheets = spreadsheet.getSheets();
+
+    sheets[0].showSheet();
+    spreadsheet.setActiveSheet(sheets[0]);
+
+    sheets.forEach(sheet => spreadsheet.deleteSheet(sheet));
+
+    spreadsheet.insertSheet();
+    spreadsheet.deleteSheet(sheets[0]);
+  }
+
+  static removeAllMetadata () {
+    Spreadsheet2.getActiveSpreadsheet()
+      .createDeveloperMetadataFinder()
+      .withVisibility(SpreadsheetApp.DeveloperMetadataVisibility.PROJECT)
+      .find()
+      .forEach(m => m.remove());
+  }
+}
