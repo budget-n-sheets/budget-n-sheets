9bcd726c9805bebee3aa717efc64c916cd866ddf
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-08-21 05:57:53 -0300

    Alter arguments of console log

--- src/LogLog.js
+++ src/LogLog.js
@@ -1,9 +1,9 @@
 class LogLog {
   static error (err) {
-    console.error(err.message + '\n' + err.stack);
+    console.error(err.message, err);
   }
 
   static warn (err) {
-    console.warn(err.message + '\n' + err.stack);
+    console.warn(err.message, err);
   }
 }
