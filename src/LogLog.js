8db273b8d56807466f4c81a7982e5fa12f05f4ab
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-09-01 18:49:40 -0300

    Fix error message formatting

--- src/LogLog.js
+++ src/LogLog.js
@@ -1,9 +1,9 @@
 class LogLog {
   static error (err) {
-    console.error(err.message, err);
+    console.error(err.message + err.stack);
   }
 
   static warn (err) {
-    console.warn(err.message, err);
+    console.warn(err.message + err.stack);
   }
 }
