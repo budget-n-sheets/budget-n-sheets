3508214c8452ba8a629471145ef797affdf70b1a
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-09-02 14:00:36 -0300

    Expands methods of Metadata

--- src/Metadata/Metadata.js
+++ src/Metadata/Metadata.js
@@ -1,37 +1,71 @@
 class Metadata {
   constructor (spreadsheet) {
     this.spreadsheet = spreadsheet || SpreadsheetApp2.getActiveSpreadsheet();
+
+    this.keys = [];
+    this.metadata = {};
+
+    this.refresh_();
+  }
+
+  add_ (key, value) {
+    this.spreadsheet.addDeveloperMetadata(
+      key, JSON.stringify(value),
+      SpreadsheetApp.DeveloperMetadataVisibility.PROJECT);
+
+    this.refresh_();
+  }
+
+  set_ (key, value) {
+    for (const id in this.metadata) {
+      if (this.metadata[id].key === key) {
+        this.metadata[id].value = value;
+        this.metadata[id].item.setValue(JSON.stringify(value));
+        break;
+      }
+    }
+  }
+
+  refresh_ () {
+    this.keys = [];
+    this.metadata = {};
+
+    this.spreadsheet.createDeveloperMetadataFinder()
+      .withVisibility(SpreadsheetApp.DeveloperMetadataVisibility.PROJECT)
+      .find()
+      .forEach(item => {
+        const key = item.getKey();
+
+        this.keys.push(key);
+        this.metadata[item.getId()] = {
+          item: item,
+          key: key,
+          value: JSON.parse(item.getValue())
+        };
+      });
+  }
+
+  getAll () {
+    return this.metadata;
   }
 
   getValueOf (key) {
-    const list = this.spreadsheet.createDeveloperMetadataFinder()
-      .withVisibility(SpreadsheetApp.DeveloperMetadataVisibility.PROJECT)
-      .withKey(key)
-      .find();
+    if (!this.hasKey(key)) return null;
 
-    return list.length > 0 ? JSON.parse(list[0].getValue()) : null;
+    for (const id in this.metadata) {
+      if (this.metadata[id].key === key) return this.metadata[id].value;
+    }
+
+    return null;
   }
 
   hasKey (key) {
-    return this.spreadsheet.createDeveloperMetadataFinder()
-      .withVisibility(SpreadsheetApp.DeveloperMetadataVisibility.PROJECT)
-      .withKey(key)
-      .find().length > 0;
+    return this.keys.indexOf(key) !== -1;
   }
 
   update (key, value) {
-    if (this.hasKey(key)) {
-      this.spreadsheet.createDeveloperMetadataFinder()
-        .withVisibility(SpreadsheetApp.DeveloperMetadataVisibility.PROJECT)
-        .withKey(key)
-        .find()[0]
-        .setValue(JSON.stringify(value))
-        .moveToSpreadsheet();
-    } else {
-      this.spreadsheet.addDeveloperMetadata(
-        key, JSON.stringify(value),
-        SpreadsheetApp.DeveloperMetadataVisibility.PROJECT);
-    }
+    if (this.hasKey(key)) this.set_(key, value);
+    else this.add_(key, value);
 
     return this;
   }
