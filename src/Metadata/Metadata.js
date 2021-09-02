6bff8da9c9c82fb1b4b8b83ec9f23e5dc79bfbe6
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-09-02 09:40:51 -0300

    Draft class Metadata

---
+++ src/Metadata/Metadata.js
@@ -0,0 +1,38 @@
+class Metadata {
+  constructor () {
+    this.spreadsheet = SpreadsheetApp2.getActiveSpreadsheet();
+  }
+
+  getValueOf (key) {
+    const list = this.spreadsheet.createDeveloperMetadataFinder()
+      .withVisibility(SpreadsheetApp.DeveloperMetadataVisibility.PROJECT)
+      .withKey(key)
+      .find();
+
+    return list.length > 0 ? JSON.parse(list[0].getValue()) : null;
+  }
+
+  hasKey (key) {
+    return this.spreadsheet.createDeveloperMetadataFinder()
+      .withVisibility(SpreadsheetApp.DeveloperMetadataVisibility.PROJECT)
+      .withKey(key)
+      .find().length > 0;
+  }
+
+  update (key, value) {
+    if (this.hasKey(key)) {
+      this.spreadsheet.createDeveloperMetadataFinder()
+        .withVisibility(SpreadsheetApp.DeveloperMetadataVisibility.PROJECT)
+        .withKey(key)
+        .find()[0]
+        .setValue(JSON.stringify(value))
+        .moveToSpreadsheet();
+    } else {
+      this.spreadsheet.addDeveloperMetadata(
+        key, JSON.stringify(value),
+        SpreadsheetApp.DeveloperMetadataVisibility.PROJECT);
+    }
+
+    return this;
+  }
+}
