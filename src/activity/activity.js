47629457b86b7b61c415bb5a7a1ad2fd6a0602bc
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-09-07 09:54:24 -0300

    Clear code

--- src/activity/activity.js
+++ src/activity/activity.js
@@ -1,35 +1,3 @@
-function toolResumeActivity_ () {
-  if (!AppsScript.isInstalled()) return;
-  if (UpdateService.checkAndUpdate(true)) return;
-
-  const name = SpreadsheetApp.getActiveSheet().getSheetName();
-  const mm = Consts.month_name.short.indexOf(name);
-
-  let mm0 = mm;
-  let mm1 = mm;
-
-  if (mm === -1) {
-    if (name !== 'Summary') {
-      SpreadsheetApp2.getUi().alert(
-        "Can't change activity",
-        'Select tab Summary or a month to resume the activity.',
-        SpreadsheetApp2.getUi().ButtonSet.OK);
-      return;
-    }
-
-    mm0 = 0;
-    mm1 = 11;
-  }
-
-  const r = switchActivity_('resume', mm0, mm1);
-
-  if (r === 1) {
-    SpreadsheetApp.getActiveSheet().toast(
-      'The add-on is busy. Try again in a moment.',
-      'Budget n Sheets');
-  }
-}
-
 function switchActivity_ (select, param1, param2) {
   const lock = LockService.getDocumentLock();
   if (!lock.tryLock(200)) return 1;
