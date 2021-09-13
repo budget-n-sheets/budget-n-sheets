bf0009cbf78ac29579e7a74972d076b011eeabae
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-09-13 11:26:32 -0300

    Add filter of ranges

--- src/tools/RefreshCashFlow.js
+++ src/tools/RefreshCashFlow.js
@@ -1,6 +1,12 @@
 class RefreshCashFlow {
   constructor () {
     this.sheet = Spreadsheet2.getSheetByName('Cash Flow');
+
+    this.indexes = new Array(12).fill(false);
+    this.specs = Object.freeze({
+      row: 4,
+      width: 3
+    });
   }
 
   static isCompatible (sheet) {
@@ -15,5 +21,27 @@ class RefreshCashFlow {
       "Can't refresh cash flow",
       'Select a month or Cash Flow to refresh the flow.',
       SpreadsheetApp2.getUi().ButtonSet.OK);
+  }
+
+  filterRanges (ranges) {
+    const name = ranges[0].getSheet().getSheetName();
+
+    if (name === 'Cash Flow') {
+      const w = specs.width + 1;
+
+      for (const range of ranges) {
+        const column = range.getColumn() - 1;
+        const last = range.getLastColumn();
+
+        for (let i = column; i < last; i += w) {
+          const index = (i - (i % w)) / w;
+          this.indexes[index] = true;
+        }
+      }
+    } else {
+      const mm = Consts.month_name.short.indexOf(name);
+      if (mm === -1) return;
+      this.indexes[mm] = true;
+    }
   }
 }
