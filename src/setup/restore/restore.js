b0d5edc5b368f10bfe71426c1d6c04f38e2b8d46
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-09-20 06:26:40 -0300

    Include header rows in sheet height

--- src/setup/restore/restore.js
+++ src/setup/restore/restore.js
@@ -263,7 +263,7 @@ function restoreCards_ (backup) {
   while (++mm < 12) {
     if (backup.cards[mm].length === 0) continue;
 
-    insertRows.insertRowsTo(backup.cards[mm].length, true);
+    insertRows.insertRowsTo(5 + backup.cards[mm].length, true);
     sheet.getRange(6, 1 + 6 * mm, backup.cards[mm].length, 5).setValues(backup.cards[mm]);
   }
 }
@@ -284,7 +284,7 @@ function restoreMonths_ (backup) {
       if (backup.ttt[mm][k] == null) continue;
       if (backup.ttt[mm][k].length === 0) continue;
 
-      insertRows.insertRowsTo(backup.ttt[mm][k].length, true);
+      insertRows.insertRowsTo(4 + backup.ttt[mm][k].length, true);
       sheet.getRange(5, 1 + 5 * k, backup.ttt[mm][k].length, 4).setValues(backup.ttt[mm][k]);
     }
   }
@@ -294,7 +294,7 @@ function restoreTags_ (backup) {
   const sheet = SpreadsheetApp2.getActiveSpreadsheet().getSheetByName('Tags');
   const insertRows = new ToolInsertRowsTags();
 
-  insertRows.insertRowsTo(backup.tags.length, true);
+  insertRows.insertRowsTo(1 + backup.tags.length, true);
 
   if (backup.tags.length > 0) {
     sheet.getRange(2, 1, backup.tags.length, 5).setValues(backup.tags);
