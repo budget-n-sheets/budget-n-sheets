4032c16e5a6795c6eb22f2c13caeac93f863831a
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-10-04 13:15:07 -0300

    Read spreadsheet settings from metadata

--- src/setup/restore/copy.js
+++ src/setup/restore/copy.js
@@ -64,11 +64,8 @@ function processSpreadsheet_ (uuid, file_id) {
   if (!FeatureFlag.getStatusOf('setup/copy')) return 1;
 
   const spreadsheet = SpreadsheetApp.openById(file_id);
-
-  let sheet, values, cols;
-  let list;
-
-  const w_ = TABLE_DIMENSION.width;
+  const metadata = new Metadata(spreadsheet);
+  let property;
 
   const info = {
     file_id: file_id,
@@ -83,49 +80,34 @@ function processSpreadsheet_ (uuid, file_id) {
     tags: 0
   };
 
-  sheet = spreadsheet.getSheetByName('_Settings');
-  if (!sheet) return 1;
+  property = metadata.getValueOf('const_properties');
+  if (property) info.financial_year = property.financial_year;
 
-  values = sheet.getRange(2, 2, 7, 1).getValues();
+  property = metadata.getValueOf('user_settings');
+  if (property) info.financial_year = property.initial_month;
 
-  info.financial_year = Number(values[0][0]);
-  info.initial_month = Number(values[2][0]) - 1;
-  info.tags = Number(values[5][0]);
-
-  sheet = spreadsheet.getSheetByName('_Backstage');
-  if (!sheet) return 1;
-
-  cols = sheet.getMaxColumns();
-  values = sheet.getRange(1, 2, 1, cols - 1).getValues();
-
-  cols = cols - 1 - 12 * w_;
-  const num_accs = (cols - (cols % w_)) / w_;
-  if (cols === 0) return 1;
-
-  list = [];
-  for (let i = 0; i < num_accs; i++) {
-    list[i] = values[0][w_ + w_ * i];
-  }
-  info.accounts = list;
-
-  list = [];
-  cols = 2 * w_ + num_accs * w_;
-  for (let i = 0; i < 10; i++) {
-    if (values[0][cols + w_ * i] !== '') {
-      const matches = values[0][cols + w_ * i].match(/\w+/g);
-      if (matches) list.push(matches);
+  property = metadata.getValueOf('db_accounts');
+  if (property) {
+    for (const k in property) {
+      info.accounts.push(property[k].name);
     }
   }
-  info.cards = list;
+
+  property = metadata.getValueOf('db_cards');
+  if (property) {
+    for (const k in property) {
+      info.cards.push(property[k].name);
+    }
+  }
+
+  const sheet = spreadsheet.getSheetByName('Tags');
+  if (sheet) info.tags = sheet.getLastRow() - 1;
 
   PropertiesService3.document().setProperty('settings_candidate', info);
 
   info.initial_month = Consts.month_name.long[info.initial_month];
 
   info.accounts = info.accounts.join(', ');
-  for (let i = 0; i < info.cards.length; i++) {
-    info.cards[i] = info.cards[i][0];
-  }
   info.cards = info.cards.join(', ');
 
   if (info.tags > 0) info.tags = 'Up to ' + info.tags + ' tag(s) found.';
