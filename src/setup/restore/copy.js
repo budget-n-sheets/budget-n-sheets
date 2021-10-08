ef0f470a5bf34542fd1ff4bbf182396fcdda6821
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-10-08 15:40:34 -0300

    Expect metadata on normal case

--- src/setup/restore/copy.js
+++ src/setup/restore/copy.js
@@ -12,7 +12,7 @@ function requestValidateSpreadsheet (uuid, file_id) {
   }
 
   if (processSpreadsheet_(uuid, file_id) !== 0) {
-    showDialogSetupCopy(uuid, 'Sorry, something went wrong. Try again in a moment.');
+    showDialogSetupCopy(uuid, 'Sorry, it was not possible to verify the spreadsheet.');
     return;
   }
 
@@ -78,30 +78,31 @@ function processSpreadsheet_ (uuid, file_id) {
   };
 
   property = metadata.getValueOf('const_properties');
-  if (property) settings_candidate.settings.financial_year = property.financial_year;
+  if (!property) return 1;
+  settings_candidate.settings.financial_year = property.financial_year;
 
   property = metadata.getValueOf('user_settings');
-  if (property) {
-    settings_candidate.settings.financial_year = property.initial_month;
-    settings_candidate.settings.financial_calendar = property.financial_calendar_sha256;
-  }
+  if (!property) return 1;
+  settings_candidate.settings.initial_month = property.initial_month;
+  settings_candidate.settings.financial_calendar = property.financial_calendar_sha256;
+
 
   property = metadata.getValueOf('db_accounts');
-  if (property) {
-    for (const k in property) {
-      settings_candidate.settings.accounts.push({
-        index: k,
-        name: property[k].name
-      });
-    }
+  if (!property) return 1;
+  for (const k in property) {
+    settings_candidate.settings.accounts.push({
+      index: k,
+      name: property[k].name
+    });
   }
 
+
   property = metadata.getValueOf('db_cards');
-  if (property) {
-    for (const k in property) {
-      settings_candidate.misc.cards.push(property[k].name);
-    }
+  if (!property) return 1;
+  for (const k in property) {
+    settings_candidate.misc.cards.push(property[k].name);
   }
+
 
   const sheet = spreadsheet.getSheetByName('Tags');
   if (sheet) settings_candidate.misc.tags = sheet.getLastRow() - 1;
