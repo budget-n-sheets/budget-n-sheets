5933dd6291447ab1a7e0ec3659a5e17fa19d730c
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2021-10-22 06:17:40 -0300

    Move backup format version to Info

--- src/Info.js
+++ src/Info.js
@@ -7,6 +7,14 @@ const Info = Object.freeze({
     }
   },
 
+  backup: {
+    version: {
+      major: 0,
+      minor: 1,
+      patch: 0
+    }
+  },
+
   template: {
     id: '',
     sheets: ['_Settings', '_Unique', 'Cards', 'Summary', 'TTT', 'Cash Flow', 'Tags', '_Backstage', '_About BnS'],
