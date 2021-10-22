2b4db7af693980cb3934e64953557d102541c8a5
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2021-10-22 17:06:41 -0300

    Add patch for backup

--- src/backup/BackupPatch.js
+++ src/backup/BackupPatch.js
@@ -8,7 +8,8 @@ class BackupPatch extends PatchThis {
     this._reference = Object.freeze(Object.assign({}, Info.backup.version));
     this._patches = [
       [
-        null, ['']
+        null,
+        [null, 'patchV0m1p1_']
       ]
     ];
 
@@ -17,6 +18,27 @@ class BackupPatch extends PatchThis {
 
   get payload () {
     return this._payload;
+  }
+
+  patchV0m1p1_ () {
+    const o = this._payload;
+
+    delete Object.assign(o, { metadata: o.backup }).backup;
+    o.metadata.spreadsheet_name = o.metadata.spreadsheet_title;
+    delete o.metadata.spreadsheet_title;
+
+    for (const k in o.db_tables.accounts) {
+      const acc = o.db_tables.accounts[k];
+      acc.time_start = acc.time_a;
+
+      delete acc.time_a;
+      delete acc.time_z;
+    }
+
+    o.user_settings.financial_calendar = o.user_settings.sha256_financial_calendar;
+    delete o.user_settings.sha256_financial_calendar;
+
+    return 0;
   }
 
   setPosition_ () {
