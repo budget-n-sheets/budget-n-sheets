d615e96a857cc3f6642cf7649e7bac44cd040bd4
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2021-10-26 07:07:18 -0300

    Bump add-on version to 0.44.3

--- src/Update/UpdateScript.js
+++ src/Update/UpdateScript.js
@@ -12,7 +12,7 @@ class UpdateScript extends Update {
         [null, null, null, update_v0m41p3_, null],
         [null, null, null, null, null, null, null, null, null, null, update_v0m42p10_, patchV0m42p11_, null, null, null, patchV0m42p15_, null, patchV0m42p17_, null, null, null, patchV0m42p21_, null, null, null, null, null, null, patchV0m42p28_, null, patchV0m42p30_, patchV0m42p31_, null, patchV0m42p33_, null, null, null, null, null, null, null, null, null, null, null],
         [null, null, null],
-        [null, null, null]
+        [null, null, null, null]
       ]
     ];
 
