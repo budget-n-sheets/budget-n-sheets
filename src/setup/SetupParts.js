7cac786ddaba69064cd7a910acd4f38eb588cbd4
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2021-11-08 09:57:45 -0300

    Change COMBO chart to COLUMN
    Apps Script issue #183272766

--- src/setup/SetupParts.js
+++ src/setup/SetupParts.js
@@ -562,9 +562,7 @@ class SetupParts {
       0: { color: '#b7b7b7', type: 'bars', labelInLegend: 'Income' },
       1: { color: '#cccccc', type: 'bars', labelInLegend: 'Expenses' },
       2: { color: '#45818e', type: 'bars', labelInLegend: 'Income' },
-      3: { color: '#e69138', type: 'bars', labelInLegend: 'Expenses' },
-      4: { color: '#45818e', type: 'line', labelInLegend: 'Avg Income' },
-      5: { color: '#e69138', type: 'line', labelInLegend: 'Avg Expenses' }
+      3: { color: '#e69138', type: 'bars', labelInLegend: 'Expenses' }
     };
 
     sheet.protect()
@@ -585,13 +583,14 @@ class SetupParts {
     sheet.getRange(11, 4, 12, 4).setFormulas(formulas);
 
     chart = sheet.newChart()
-      .addRange(sheet.getRange('C25:I36'))
-      .setChartType(Charts.ChartType.COMBO)
+      .addRange(sheet.getRange('C25:G36'))
+      .setChartType(Charts.ChartType.COLUMN)
       .setPosition(24, 2, 0, 0)
       .setOption('mode', 'view')
       .setOption('legend', 'top')
       .setOption('focusTarget', 'category')
       .setOption('series', options)
+      .setOption('vAxis.minorGridlines.count', 3)
       .setOption('height', 482)
       .setOption('width', 886);
 
