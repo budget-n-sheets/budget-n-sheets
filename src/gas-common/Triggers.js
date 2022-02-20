f2c531f1f80791404312068328af00ba2f16899d
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-02-20 10:54:18 -0300

    Add method to delete trigger by type

--- src/gas-common/Triggers.js
+++ src/gas-common/Triggers.js
@@ -1,7 +1,15 @@
 class Triggers {
+  static get triggers () {
+    return ScriptApp.getUserTriggers(SpreadsheetApp2.getActiveSpreadsheet());
+  }
+
   static deleteAllUserTriggers () {
-    ScriptApp.getUserTriggers(SpreadsheetApp2.getActiveSpreadsheet()).forEach(
-      trigger => ScriptApp.deleteTrigger(trigger)
-    );
+    this.triggers.forEach(trigger => ScriptApp.deleteTrigger(trigger));
+  }
+
+  static deleteTriggerByEventType (eventType) {
+    this.triggers.forEach(trigger => {
+      if (trigger.getEventType() === eventType) ScriptApp.deleteTrigger(trigger);
+    });
   }
 }
