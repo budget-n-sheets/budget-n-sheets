54f8d7b305f82d6cf25ccfbc82e04e455db14663
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-03-17 09:03:14 -0300

    Insert charts with SheetSummaryCharts

--- src/setup/SetupParts.js
+++ src/setup/SetupParts.js
@@ -588,14 +588,6 @@ class SetupParts {
       ['Month', 'Income', 'Expenses', 'Income', 'Expenses', 'Avg Income', 'Avg Expenses']
     ]);
 
-    try {
-      const request = { addChart: { chart: { position: { overlayPosition: { widthPixels: 886, heightPixels: 482, anchorCell: { sheetId: sheetId, rowIndex: 23, columnIndex: 1 } } }, spec: { hiddenDimensionStrategy: 'SKIP_HIDDEN_ROWS_AND_COLUMNS', basicChart: { headerCount: 1, chartType: 'COMBO', legendPosition: 'TOP_LEGEND', compareMode: 'CATEGORY', interpolateNulls: true, domains: [{ domain: { sourceRange: { sources: [{ sheetId: sheetId, startRowIndex: 23, endRowIndex: 36, startColumnIndex: 2, endColumnIndex: 3 }] } } }], series: [{ type: 'COLUMN', targetAxis: 'LEFT_AXIS', color: { red: 183 / 255, green: 183 / 255, blue: 183 / 255 }, colorStyle: { rgbColor: { red: 183 / 255, green: 183 / 255, blue: 183 / 255 } }, series: { sourceRange: { sources: [{ sheetId: sheetId, startRowIndex: 23, endRowIndex: 36, startColumnIndex: 3, endColumnIndex: 4 }] } } }, { type: 'COLUMN', targetAxis: 'LEFT_AXIS', color: { red: 204 / 255, green: 204 / 255, blue: 204 / 255 }, colorStyle: { rgbColor: { red: 204 / 255, green: 204 / 255, blue: 204 / 255 } }, series: { sourceRange: { sources: [{ sheetId: sheetId, startRowIndex: 23, endRowIndex: 36, startColumnIndex: 4, endColumnIndex: 5 }] } } }, { type: 'COLUMN', targetAxis: 'LEFT_AXIS', color: { red: 69 / 255, green: 129 / 255, blue: 142 / 255 }, colorStyle: { rgbColor: { red: 69 / 255, green: 129 / 255, blue: 142 / 255 } }, series: { sourceRange: { sources: [{ sheetId: sheetId, startRowIndex: 23, endRowIndex: 36, startColumnIndex: 5, endColumnIndex: 6 }] } } }, { type: 'COLUMN', targetAxis: 'LEFT_AXIS', color: { red: 230 / 255, green: 145 / 255, blue: 56 / 255 }, colorStyle: { rgbColor: { red: 230 / 255, green: 145 / 255, blue: 56 / 255 } }, series: { sourceRange: { sources: [{ sheetId: sheetId, startRowIndex: 23, endRowIndex: 36, startColumnIndex: 6, endColumnIndex: 7 }] } } }, { type: 'LINE', targetAxis: 'LEFT_AXIS', color: { red: 69 / 255, green: 129 / 255, blue: 142 / 255 }, colorStyle: { rgbColor: { red: 69 / 255, green: 129 / 255, blue: 142 / 255 } }, series: { sourceRange: { sources: [{ sheetId: sheetId, startRowIndex: 23, endRowIndex: 36, startColumnIndex: 7, endColumnIndex: 8 }] } } }, { type: 'LINE', targetAxis: 'LEFT_AXIS', color: { red: 230 / 255, green: 145 / 255, blue: 56 / 255 }, colorStyle: { rgbColor: { red: 230 / 255, green: 145 / 255, blue: 56 / 255 } }, series: { sourceRange: { sources: [{ sheetId: sheetId, startRowIndex: 23, endRowIndex: 36, startColumnIndex: 8, endColumnIndex: 9 }] } } }] } } } } };
-
-      Sheets.Spreadsheets.batchUpdate({ requests: [request] }, this._spreadsheetId);
-    } catch (err) {
-      LogLog.error(err);
-    }
-
     if (this._config.decimal_places !== 2) {
       sheet.getRangeList(['D9:I22', 'D25:G36']).setNumberFormat(this._config.number_format);
     }
@@ -603,52 +595,16 @@ class SetupParts {
     formula = formulaBuild.table2().data();
     sheet.getRange(55, 2).setFormula(formula);
 
-    try {
-      chart = sheet.newChart()
-        .addRange(sheet.getRange('B54:B64'))
-        .addRange(sheet.getRange('D54:D64'))
-        .setNumHeaders(1)
-        .setChartType(Charts.ChartType.PIE)
-        .setPosition(52, 8, 0, 0)
-        .setOption('mode', 'view')
-        .setOption('legend', 'top')
-        .setOption('focusTarget', 'category')
-        .setOption('height', 447)
-        .setOption('width', 444)
-        .build();
-
-      sheet.insertChart(chart);
-    } catch (err) {
-      LogLog.error(err);
-    }
+    sheet.getRange(74, 9, 1, 3).setValues([
+      ['Total', 'Total', 'Average']
+    ]);
 
     formula = formulaBuild.table3().total();
     sheet.getRange(75, 4).setFormula(formula);
 
-    options = {
-      0: { color: '#b7b7b7', type: 'bars', labelInLegend: 'Total' },
-      1: { color: '#45818e', type: 'bars', labelInLegend: 'Total' },
-      2: { color: '#45818e', type: 'line', labelInLegend: 'Average' }
-    };
-
-    try {
-      chart = sheet.newChart()
-        .addRange(sheet.getRange('B75:B86'))
-        .addRange(sheet.getRange('I75:K86'))
-        .setChartType(Charts.ChartType.COMBO)
-        .setPosition(72, 8, 0, 0)
-        .setOption('mode', 'view')
-        .setOption('legend', 'top')
-        .setOption('focusTarget', 'category')
-        .setOption('series', options)
-        .setOption('height', 459)
-        .setOption('width', 444)
-        .build();
-
-      sheet.insertChart(chart);
-    } catch (err) {
-      LogLog.error(err);
-    }
+    new SheetSummaryCharts().insertChart1()
+      .insertChart2()
+      .insertChart3();
 
     SpreadsheetApp.flush();
   }
