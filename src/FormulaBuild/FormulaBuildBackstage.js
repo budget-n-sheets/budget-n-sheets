196e86d6ed584a99371f885f6a71d33950640197
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-06-14 07:26:07 -0300

    Add function to FormulaBuild

--- src/FormulaBuild/FormulaBuildBackstage.js
+++ src/FormulaBuild/FormulaBuildBackstage.js
@@ -169,6 +169,31 @@ class FormulaBuildBackstageCards {
     return formula;
   }
 
+  static cardDue (numRows, mm, regex, bsblank) {
+    this.load_();
+
+    const transaction = RangeUtils.rollA1Notation(6, 2 + 6 * mm, numRows);
+    const card = RangeUtils.rollA1Notation(6, 3 + 6 * mm, numRows);
+    const value = RangeUtils.rollA1Notation(6, 4 + 6 * mm, numRows);
+
+    const dec_s = this._settings.decimal_separator ? ',' : '\\';
+
+    let formula;
+
+    formula = `REGEXEXTRACT(ARRAY_CONSTRAIN(Cards!${transaction}; ${bsblank}; 1); "[0-9]+/[0-9]+")`;
+    formula = `ARRAYFORMULA(SPLIT(${formula}; "/"))`;
+
+    formula = `{${formula}${dec_s} ARRAY_CONSTRAIN(Cards!${value}; ${bsblank}; 1)}; `;
+    formula += `REGEXMATCH(ARRAY_CONSTRAIN(Cards!${card}; ${bsblank}; 1); ${regex}); `;
+    formula += `REGEXMATCH(ARRAY_CONSTRAIN(Cards!${transaction}; ${bsblank}; 1); "[0-9]+/[0-9]+"); `;
+    formula += `NOT(REGEXMATCH(ARRAY_CONSTRAIN(Cards!${transaction}; ${bsblank}; 1); "[^0-9\\s][0-9]+/[0-9]+"))`;
+
+    formula = `QUERY(FILTER(${formula}), "SELECT (Col2 - Col1) * Col3 WHERE Col1 < Col2 LABEL (Col2 - Col1) * Col3 ''")`
+    formula = `IF(${regex} = ""; 0; SUM(IFNA(${formula}; 0)))`;
+
+    return formula;
+  }
+
   static bsblank (numRows, mm) {
     const header = RangeUtils.rollA1Notation(5, 4 + 6 * mm);
     return 'MIN(ARRAYFORMULA(IF(ISBLANK(Cards!' + RangeUtils.rollA1Notation(6, 4 + 6 * mm, numRows, 1) + '); ROW(Cards!' + RangeUtils.rollA1Notation(6, 4 + 6 * mm, numRows, 1) + ') - ROW(Cards!' + header + '); FALSE)); ROWS(Cards!' + RangeUtils.rollA1Notation(6, 4 + 6 * mm, numRows, 1) + '))';
