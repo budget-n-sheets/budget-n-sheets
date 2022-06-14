b7098d574920056e1c2fafbe2581e4ef911ba590
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-06-14 08:09:35 -0300

    Re-issue script patch v0m46p5

--- src/Update/UpdateScript.js
+++ src/Update/UpdateScript.js
@@ -14,7 +14,7 @@ class UpdateScript extends Update {
         ['', '', ''],
         ['', '', '', ''],
         ['', 'patchV0m45p1_', '', '', 'patchV0m45p4_', '', '', 'patchV0m45p7_', '', '', '', '', '', '', '', ''],
-        ['', '', 'patchV0m46p2_', 'patchV0m46p3_', '', 'patchV0m46p5_']
+        ['', '', 'patchV0m46p2_', 'patchV0m46p3_', '', '', 'patchV0m46p6_']
       ]
     ];
 
@@ -26,9 +26,9 @@ class UpdateScript extends Update {
   /**
    * Resume activity to update functions.
    *
-   * 0.46.5
+   * 0.46.6
    */
-   patchV0m46p5_ () {
+   patchV0m46p6_ () {
     if (!Spreadsheet2.getSheetByName('_Backstage')) return 3;
 
     try {
