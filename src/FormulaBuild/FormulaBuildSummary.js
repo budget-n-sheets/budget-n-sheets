37eda69556b61ad9f448ac8b0a1e6847f7a5e2cd
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-06-16 07:02:03 -0300

    Update ranges references

--- src/FormulaBuild/FormulaBuildSummary.js
+++ src/FormulaBuild/FormulaBuildSummary.js
@@ -10,17 +10,17 @@ class FormulaBuildSummary {
 
 class FormulaBuildSummaryTable1 {
   static income () {
-    return 'IF(_Settings!$B6 > 0;  {SUM(OFFSET($D10; _Settings!$B4; 0; _Settings!$B6; 1)); AVERAGE(OFFSET($D10; _Settings!$B4; 0; _Settings!$B6; 1))}; {0; 0})';
+    return 'IF(_Settings!B6 > 0;  {SUM(OFFSET(D8; _Settings!B4; 0; _Settings!B6; 1)); AVERAGE(OFFSET(D8; _Settings!B4; 0; _Settings!B6; 1))}; {0; 0})';
   }
 
   static expenses () {
-    return 'IF(_Settings!$B6 > 0;  {SUM(OFFSET($F10; _Settings!$B4; 0; _Settings!$B6; 1)); AVERAGE(OFFSET($F10; _Settings!$B4; 0; _Settings!$B6; 1))}; {0; 0})';
+    return 'IF(_Settings!B6 > 0;  {SUM(OFFSET(F8; _Settings!B4; 0; _Settings!B6; 1)); AVERAGE(OFFSET(F8; _Settings!B4; 0; _Settings!B6; 1))}; {0; 0})';
   }
 
   static expensesMonth (mm) {
     const _h = TABLE_DIMENSION.height;
 
-    const formula = 'SUM(_Backstage!$B' + (4 + _h * mm) + ':$B' + (6 + _h * mm) + ')';
+    const formula = 'SUM(_Backstage!B' + (4 + _h * mm) + ':B' + (6 + _h * mm) + ')';
 
     return formula;
   }
@@ -36,9 +36,9 @@ class FormulaBuildSummaryChart1 {
 
     const dec_s = this._settings.decimal_separator ? ',' : '\\';
 
-    const income = RangeUtils.rollA1Notation(11 + mm, 4);
-    const expenses = RangeUtils.rollA1Notation(11 + mm, 6);
+    const income = RangeUtils.rollA1Notation(9 + mm, 4);
+    const expenses = RangeUtils.rollA1Notation(9 + mm, 6);
 
-    return 'IF(OR(ROW() - 24 < $M$3; ROW() - 24 > $M$3 - 1 + $M$4); {' + income + dec_s + ' -' + expenses + dec_s + ' ""' + dec_s + ' ""}; {""' + dec_s + ' ""' + dec_s + ' ' + income + dec_s + ' -' + expenses + '})';
+    return `IF(OR(ROW() - 23 < '_Settings'!B4; ROW() - 23 > '_Settings'!B4 - 1 + '_Settings'!B6); {${income}${dec_s} -${expenses}${dec_s} ""${dec_s} ""}; {""${dec_s} ""${dec_s} ${income}${dec_s} -${expenses}})`;
   }
 }
