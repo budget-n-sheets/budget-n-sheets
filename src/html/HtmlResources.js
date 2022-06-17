6b537143985de39388c152db8490d4d70d12edea
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-06-17 06:18:39 -0300

    Add global reference to HTML resources

---
+++ src/html/HtmlResources.js
@@ -0,0 +1,5 @@
+const HtmlResources = Object.freeze({
+  add_ons_css: "https://ssl.gstatic.com/docs/script/css/add-ons1.css",
+  material_icons: "https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined",
+  jquery_min: "https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"
+});
