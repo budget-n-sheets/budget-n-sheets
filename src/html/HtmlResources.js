5fc13092823d719d22afbef7172ee61fa136f3b6
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-06-17 06:42:31 -0300

    Group HTML resources by type

--- src/html/HtmlResources.js
+++ src/html/HtmlResources.js
@@ -1,5 +1,11 @@
 const HtmlResources = Object.freeze({
-  add_ons_css: "https://ssl.gstatic.com/docs/script/css/add-ons1.css",
-  material_icons: "https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined",
-  jquery_min: "https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"
+  css: {
+    add_ons: "https://ssl.gstatic.com/docs/script/css/add-ons1.css"
+  },
+  fonts: {
+    material_icons: "https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined"
+  },
+  js: {
+    jquery_min: "https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"
+  }
 });
