9626447b3a77e4d02baf376003f105ddd06aafe1
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-06-22 18:51:27 -0300

    Add 'color_palette' to Consts

--- src/Consts.js
+++ src/Consts.js
@@ -11,4 +11,21 @@ class Consts {
       long: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     };
   }
+
+  static get color_palette () {
+    return {
+      whitesmoke: 'f5f5f5',
+      slategray: '2f4f4f',
+      black: '000000',
+      darkblue: '00008b',
+      slateblue: '483d8b',
+      lightskyblue: '87cefa',
+      seagreen: '2e8b57',
+      mediumseagreen: '3cb371',
+      crimson: 'dc143c',
+      deeppink: 'ff1493',
+      darkorange: 'ff8c00',
+      goldenrod: 'daa520'
+    };
+  }
 }
