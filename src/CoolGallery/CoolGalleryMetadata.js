8808eda6873d9cebf82543101fb657614e728521
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-06-27 14:25:33 -0300

    Suspend template Stats for Tags

--- src/CoolGallery/CoolGalleryMetadata.js
+++ src/CoolGallery/CoolGalleryMetadata.js
@@ -11,22 +11,22 @@ class CoolGalleryMetadata {
     };
   }
 
-  static getStatsForTags () {
-    return {
-      template_id: '',
-      preview_id: '',
-      version_name: 'v1.0.1',
-      version_date: '2020-02-25',
-      name: 'Stats for Tags',
-      description: 'View stats for your tags by month, category, and tags.',
-      sheets: ['Stats for Tags']
-    };
-  }
+  // static getStatsForTags () {
+  //   return {
+  //     template_id: '',
+  //     preview_id: '',
+  //     version_name: 'v1.0.1',
+  //     version_date: '2020-02-25',
+  //     name: 'Stats for Tags',
+  //     description: 'View stats for your tags by month, category, and tags.',
+  //     sheets: ['Stats for Tags']
+  //   };
+  // }
 
   static getAll () {
     return {
-      filter_by_tag: this.getFilterByTag(),
-      stats_for_tags: this.getStatsForTags()
+      filter_by_tag: this.getFilterByTag()
+      // stats_for_tags: this.getStatsForTags()
     };
   }
 }
