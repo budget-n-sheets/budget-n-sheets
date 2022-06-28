c9d5c51c5756e708356b1667b3b596775a1b79c4
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-06-28 15:21:56 -0300

    Add MirrorSheet

---
+++ src/MirrorSheet.js
@@ -0,0 +1,40 @@
+class MirrorSheet {
+  constructor (metadata) {
+    this._spreadsheet = SpreadsheetApp2.getActiveSpreadsheet();
+    this._metadata = metadata;
+
+    this._consts = {};
+    this._settings = {};
+  }
+
+  get metadata () {
+    return this._metadata;
+  }
+
+  get name () {
+    return this._metadata.name;
+  }
+
+  get sheet () {
+    return this._sheet ||
+          (this._sheet = Spreadsheet2.getSheetByName(this.name));
+  }
+
+  copyTemplate () {
+    SpreadsheetService.copySheetsFromSource(this._metadata.id, [this.name]);
+    SpreadsheetApp.flush();
+    return this;
+  }
+
+  deleteTemplate () {
+    const sheet = Spreadsheet2.getSheetByName(this.name);
+    if (sheet) this._spreadsheet.deleteSheet(this.sheet);
+    this._sheet = null;
+    SpreadsheetApp.flush();
+    return this;
+  }
+
+  isInstalled () {
+    return this.sheet != null;
+  }
+}
