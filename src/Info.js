c0a4f79d18762103d8c01b5faeee519ee914e847
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-06-29 10:19:31 -0300

    Bump template version to 0.14.0

--- src/Info.js
+++ src/Info.js
@@ -20,8 +20,8 @@ const Info = Object.freeze({
     sheets: ['_Settings', '_Unique', 'Cards', 'Summary', 'TTT', 'Cash Flow', 'Tags', '_Backstage', '_About BnS'],
     version: {
       major: 0,
-      minor: 13,
-      patch: 5
+      minor: 14,
+      patch: 0
     }
   }
 });
