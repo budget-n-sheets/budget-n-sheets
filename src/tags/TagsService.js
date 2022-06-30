a63f655deb5348e2b28961126b63147ad2b5d0a8
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-06-30 13:58:06 -0300

    Improve search for data validation rule

--- src/tags/TagsService.js
+++ src/tags/TagsService.js
@@ -5,17 +5,27 @@ class TagsService {
 
     if (sheet.getMaxRows() < 2) return Consts.tags_categories;
 
-    const categories = sheet.getRange('B2').getDataValidation()?.getCriteriaValues()[0] || Consts.tags_categories;
-    return Array.isArray(categories) ? categories : Consts.tags_categories;
+    let categories = sheet.getRange('B2:B').getValues().map(r => r[0]).filter(v => v);
+
+    const validations = sheet.getRange('B2:B').getDataValidations().map(r => r[0]).filter(v => v);
+    for (const validation of validations) {
+      if (validation.getCriteriaType() !== SpreadsheetApp.DataValidationCriteria.VALUE_IN_LIST) continue;
+      const values = validation.getCriteriaValues()[0];
+      if (values.length === 0) continue;
+      categories = values.concat(categories);
+      break;
+    }
+
+    categories = categories.filter((v, i, s) => s.indexOf(v) === i);
+
+    return categories.length > 0 ? categories : Consts.tags_categories;
   }
 
   static setCategories (categories) {
     const sheet = Spreadsheet2.getSheetByName('Tags');
     if (!sheet) return;
 
-    categories = categories.filter(c => c && c != null)
-      .map(c => c.trim())
-      .filter((v, i, s) => v && s.indexOf(v) === i);
+    categories = categories.map(c => c.trim()).filter((v, i, s) => v && s.indexOf(v) === i);
     if (categories.length < 1) categories = ['Other'];
 
     const rule = SpreadsheetApp.newDataValidation()
