1ff8eae4fac249fbd22e7bb002c6afecf8817b26
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-08-11 15:31:49 -0300

    Update jQuery libraries

--- src/html/HtmlResources.js
+++ src/html/HtmlResources.js
@@ -7,7 +7,7 @@ const HtmlResources = Object.freeze({
     material_icons: "https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined"
   },
   js: {
-    jquery_min: "https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js",
-    jquery_ui_min: "https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"
+    jquery_min: "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js",
+    jquery_ui_min: "https://ajax.googleapis.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js"
   }
 });
