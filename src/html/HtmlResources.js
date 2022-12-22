fb7f67edbcec1caafe9042510c000a52bc51ea67
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-12-22 15:50:02 -0300

    Bump jQuery versions

--- src/html/HtmlResources.js
+++ src/html/HtmlResources.js
@@ -1,13 +1,13 @@
 const HtmlResources = Object.freeze({
   css: {
     add_ons: "https://ssl.gstatic.com/docs/script/css/add-ons1.css",
-    jquery_ui: "https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css"
+    jquery_ui: "https://ajax.googleapis.com/ajax/libs/jqueryui/1.13.2/themes/smoothness/jquery-ui.css"
   },
   fonts: {
     material_icons: "https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined"
   },
   js: {
-    jquery_min: "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js",
+    jquery_min: "https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js",
     jquery_ui_min: "https://ajax.googleapis.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js"
   },
   href: {
