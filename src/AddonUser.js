5149f99fa09422923589b4e210f154e0eeba4fb9
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-12-30 11:08:23 -0300

    Add AddonUser class

---
+++ src/AddonUser.js
@@ -0,0 +1,21 @@
+/**
+ * Budget n Sheets Copyright 2017-2022 Guilherme T Maeoka
+ * <https://github.com/budget-n-sheets/budget-n-sheets>
+ *
+ * This program comes with ABSOLUTELY NO WARRANTY.
+ * This is free software, and you are welcome to redistribute it
+ * under certain conditions.
+ * <https://www.gnu.org/licenses/>
+ */
+
+class AddonUser {
+  static getAccessLevel () {
+    const id = SpreadsheetApp2.getActive().getId()
+    const permission = new DriveFile(id).getUserPermission()
+    return DriveRoles.getRoleLevel(permission.role)
+  }
+
+  static hasBaselinePermission () {
+    return this.getAccessLevel() <= 2
+  }
+}
