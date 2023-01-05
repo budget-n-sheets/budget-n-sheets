9e135ffa839d922ab3e1512d564cad5023fe3ad3
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2023-01-05 16:47:31 -0300

    Move method 'isLocked'

--- src/Bns/BnsTemplate.js
+++ src/Bns/BnsTemplate.js
@@ -19,10 +19,6 @@ class BnsTemplate {
     return true;
   }
 
-  static isLocked () {
-    return !!PropertiesService2.getDocumentProperties().getProperty('lock_spreadsheet');
-  }
-
   static isUpToDate () {
     const v0 = ClassVersion.getValueOf('template');
     const vA = Info.template.version;
