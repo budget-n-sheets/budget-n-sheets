d8ea2a894e7d54f97f420cd84dbd13ff09754266
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2023-10-01 01:26:07 -0300

    Update ViewMode

--- src/ViewMode/ViewModeCompact.js
+++ src/ViewMode/ViewModeCompact.js
@@ -10,26 +10,15 @@
 
 class ViewModeCompact {
   static compactTtt_ () {
-    const num_acc = SettingsConst.get('number_accounts');
-
     for (let i = 0; i < 12; i++) {
       const sheet = SpreadsheetApp2.getActive().getSheetByName(Consts.month_name.short[i]);
       if (!sheet) continue;
       if (sheet.getMaxRows() < 3) continue;
 
-      let range = sheet.getRange(1, 3, 1, 2);
+      let range = sheet.getRange(1, 5, 1, 2);
       range.offset(0, 0, 3, 2).breakApart();
       range.merge().setFormulaR1C1('R[2]C[-2]');
       range.offset(0, -2).setBorder(null, null, false, null, null, null);
-
-      range = sheet.getRange(1, 8, 1, 2);
-      for (let k = 0; k < num_acc; k++) {
-        range.offset(0, 5 * k, 3, 2).breakApart();
-        range.offset(0, 5 * k)
-          .merge()
-          .setFormulaR1C1('R[2]C[-2]');
-        range.offset(0, -2 + 5 * k).setBorder(null, null, false, null, null, null);
-      }
 
       sheet.hideRows(2, 2);
     }
--- src/ViewMode/ViewModeNormal.js
+++ src/ViewMode/ViewModeNormal.js
@@ -11,7 +11,6 @@
 class ViewModeNormal {
   static expandTtt_ () {
     const formulas = FormulaBuildTtt.header();
-    const num_acc = SettingsConst.get('number_accounts');
 
     for (let i = 0; i < 12; i++) {
       const sheet = SpreadsheetApp2.getActive().getSheetByName(Consts.month_name.short[i]);
@@ -20,19 +19,12 @@ class ViewModeNormal {
 
       sheet.showRows(2, 2);
 
-      let range = sheet.getRange(1, 1, 1, 2);
+      let range = sheet.getRange(1, 3, 1, 2);
       range.offset(0, 2, 3, 2)
         .merge()
-        .clearContent();
+        // TODO
+        // .setFormula(formulas.report(k, i))
       range.setBorder(null, null, true, null, null, null, '#000000', SpreadsheetApp.BorderStyle.SOLID_MEDIUM);
-
-      range = range.offset(0, 5);
-      for (let k = 0; k < num_acc; k++) {
-        range.offset(0, 2 + 5 * k, 3, 2)
-          .merge()
-          .setFormula(formulas.report(k, i));
-        range.offset(0, 5 * k).setBorder(null, null, true, null, null, null, '#000000', SpreadsheetApp.BorderStyle.SOLID_MEDIUM);
-      }
     }
 
     SpreadsheetApp.flush();
