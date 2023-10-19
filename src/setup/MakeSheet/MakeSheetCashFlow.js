c35bab156a9e729c0ead7e6627176a1eaff93033
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2023-10-19 02:54:47 -0300

    Draft MakeSheetCashFlow

---
+++ src/setup/MakeSheet/MakeSheetCashFlow.js
@@ -0,0 +1,31 @@
+/**
+ * Budget n Sheets Copyright 2017-2023 Guilherme T Maeoka
+ * <https://github.com/budget-n-sheets/budget-n-sheets>
+ *
+ * This program comes with ABSOLUTELY NO WARRANTY.
+ * This is free software, and you are welcome to redistribute it
+ * under certain conditions.
+ * <https://www.gnu.org/licenses/>
+ */
+
+class MakeSheetCashFlow extends MakeSheet {
+  constructor () {
+    super(MakeSheetCashFlow.metadata)
+  }
+
+  static get metadata () {
+    return {
+      name: 'Cash Flow',
+      requires: ['_Backstage']
+    }
+  }
+
+  make () {
+    new SheetCashFlow().resetDefault()
+    SpreadsheetApp.flush()
+  }
+
+  makeConfig () {
+    return this
+  }
+}
