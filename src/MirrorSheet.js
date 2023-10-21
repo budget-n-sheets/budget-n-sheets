4d0f63a6e4ee8ec3b3bb67dc24b03a1267fe1938
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2023-10-20 22:25:13 -0300

    Change MirroSheet data structure

--- src/MirrorSheet.js
+++ src/MirrorSheet.js
@@ -9,37 +9,33 @@
  */
 
 class MirrorSheet {
-  constructor (metadata) {
-    this._spreadsheet = SpreadsheetApp2.getActive().spreadsheet;
-    this._metadata = metadata;
+  constructor (name, requires, template) {
+    this._template = {}
+    Object.assign(this._template, template)
 
-    this._consts = {};
-    this._settings = {};
-  }
-
-  get metadata () {
-    return this._metadata;
+    this._name = name
+    this._requires = requires
   }
 
   get name () {
-    return this._metadata.name;
+    return this._name;
   }
 
   get sheet () {
     return this._sheet ||
-          (this._sheet = SpreadsheetApp2.getActive().getSheetByName(this.name));
+          (this._sheet = SpreadsheetApp2.getActive().getSheetByName(this._name));
   }
 
   copyTemplate () {
-    const source = SpreadsheetApp.openById(this._metadata.id);
-    SpreadsheetApp2.getActive().copySheetsFrom(source, [this.name]);
+    const source = SpreadsheetApp.openById(this._template.id);
+    SpreadsheetApp2.getActive().copySheetsFrom(source, [this._name]);
     SpreadsheetApp.flush();
     return this;
   }
 
   deleteTemplate () {
-    const sheet = SpreadsheetApp2.getActive().getSheetByName(this.name);
-    if (sheet) this._spreadsheet.deleteSheet(this.sheet);
+    const sheet = SpreadsheetApp2.getActive().getSheetByName(this._name);
+    if (sheet) SpreadsheetApp2.getActive().deleteSheet(this.sheet);
     this._sheet = null;
     SpreadsheetApp.flush();
     return this;
