0504d8b66ee238dbfd4837adfdd8c9c1d27e7660
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2023-10-24 20:48:16 -0300

    Draft RefreshCashFlowService

---
+++ src/tools/RefreshCashFlow/RefreshCashFlowService.js
@@ -0,0 +1,49 @@
+/**
+ * Budget n Sheets Copyright 2017-2023 Guilherme T Maeoka
+ * <https://github.com/budget-n-sheets/budget-n-sheets>
+ *
+ * This program comes with ABSOLUTELY NO WARRANTY.
+ * This is free software, and you are welcome to redistribute it
+ * under certain conditions.
+ * <https://www.gnu.org/licenses/>
+ */
+
+class RefreshCashFlowService {
+  static isCompatible (sheet) {
+    const name = sheet.getName()
+    return name === 'Cash Flow' ||
+           Consts.month_name.short.indexOf(name) > -1
+  }
+
+  static serve (sheet, ranges) {
+    if (!this.isCompatible(sheet)) {
+      this.showWarning()
+      return
+    }
+
+    if (!SpreadsheetApp2.getActive().getSheetByName('Cash Flow')) {
+      this.showMissing()
+      return
+    }
+
+    const indexes = RefreshCashFlow.filterRanges(ranges)
+    if (indexes.length === 0) return
+
+    const tool = new RefreshCashFlow()
+    indexes.forEach(index => tool.refresh(index))
+  }
+
+  static showMissing () {
+    SpreadsheetApp2.getUi().alert(
+      "Can't refresh cash flow",
+      'Sheet Cash Flow not found.',
+      SpreadsheetApp2.getUi().ButtonSet.OK)
+  }
+
+  static showWarning () {
+    SpreadsheetApp2.getUi().alert(
+      "Can't refresh cash flow",
+      'Select a month or Cash Flow to refresh the flow.',
+      SpreadsheetApp2.getUi().ButtonSet.OK)
+  }
+}
