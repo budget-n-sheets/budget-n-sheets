71c6f0e767def89c58b492115eb152440dbe1db8
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2023-10-24 21:53:43 -0300

    Draft MonthTableUtils

---
+++ src/ExtendedSheet/Month/MonthTableUtils.js
@@ -0,0 +1,27 @@
+/**
+ * Budget n Sheets Copyright 2017-2023 Guilherme T Maeoka
+ * <https://github.com/budget-n-sheets/budget-n-sheets>
+ *
+ * This program comes with ABSOLUTELY NO WARRANTY.
+ * This is free software, and you are welcome to redistribute it
+ * under certain conditions.
+ * <https://www.gnu.org/licenses/>
+ */
+
+class MonthTableUtils {
+  static sliceBlankRow (values) {
+    let n = values.length
+    while (--n > -1) {
+      if (values[n].findIndex(e => e !== '') > -1) break
+    }
+    n++
+    return n > 0 ? values.slice(0, n) : []
+  }
+
+  static sliceBlankValue (values) {
+    const nil = SheetMonth.specs.nullSearch - 1
+    const n = values.findIndex(row => row[nil] === '')
+    if (n === -1) return values
+    return n > 0 ? values.slice(0, n) : []
+  }
+}
