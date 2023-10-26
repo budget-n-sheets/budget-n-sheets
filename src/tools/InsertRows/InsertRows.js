8f09b4d127d2981f9732bd783f325de7a848821c
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2023-10-25 21:48:57 -0300

    Revamp Insert Rows

---
+++ src/tools/InsertRows/InsertRows.js
@@ -0,0 +1,52 @@
+/**
+ * Budget n Sheets Copyright 2017-2023 Guilherme T Maeoka
+ * <https://github.com/budget-n-sheets/budget-n-sheets>
+ *
+ * This program comes with ABSOLUTELY NO WARRANTY.
+ * This is free software, and you are welcome to redistribute it
+ * under certain conditions.
+ * <https://www.gnu.org/licenses/>
+ */
+
+class InsertRows {
+  static getSheetSpecs_ (sheet) {
+    const name = sheet.getSheetName()
+    if (name === 'Tags') return SheetTags.specs
+    else if (Consts.month_name.short.indexOf(name) > -1) return SheetMonth.specs
+    else throw new Error('Specifications not found.')
+  }
+
+  static insertNumRows_ (sheet, specs, numRows = 400) {
+    let maxRows = sheet.getMaxRows()
+    if (maxRows < specs.row) throw new Error('Bad sheet structure.')
+
+    sheet.insertRowsBefore(maxRows, numRows)
+    maxRows += numRows
+
+    if (sheet.getLastRow() === maxRows) {
+      const maxCols = sheet.getMaxColumns()
+      const range = sheet.getRange(maxRows, 1, 1, maxCols)
+      const values = range.getValues()
+
+      range.clearContent()
+        .offset(0 - numRows, 0)
+        .setValues(values)
+    }
+
+    SpreadsheetApp.flush()
+  }
+
+  static insertRows (sheet) {
+    const specs = this.getSheetSpecs_(sheet)
+
+    this.insertNumRows_(sheet, specs)
+  }
+
+  static insertRowsTo (sheet, height, extras = false) {
+    const maxRows = sheet.getMaxRows()
+    if (maxRows >= height) return
+
+    const specs = this.getSheetSpecs_(sheet)
+    this.insertNumRows_(sheet, specs, height - maxRows + (!!extras * 100))
+  }
+}
