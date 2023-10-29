c63d558587b11574771f603b598de661b1570b7d
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2023-10-29 01:54:22 -0300

    Clean up code

--- src/quickstart/quickstart.js
+++ src/quickstart/quickstart.js
@@ -8,13 +8,6 @@
  * <https://www.gnu.org/licenses/>
  */
 
-function alertQuickstartSheetMissing (name) {
-  SpreadsheetApp2.getUi().alert(
-    "Can't show example",
-    'Sheet "' + name + "\" couldn't be found.",
-    SpreadsheetApp2.getUi().ButtonSet.OK)
-}
-
 function playQuickstart (id) {
   if (!Addon.isInstalled()) return
 
@@ -33,65 +26,4 @@ function playQuickstart (id) {
   QuickstartPl.ay(name, num)
 
   lock.releaseLock()
-}
-
-function fillMonthWithZeros (sheet) {
-  let lastRow
-  let i, k
-
-  lastRow = sheet.getLastRow()
-  if (lastRow < 5) return
-
-  lastRow -= 4
-  const values = sheet.getRange(5, 1, lastRow, 10).getValues()
-
-  let n = 0
-  const list = []
-
-  for (k = 0; k < 2; k++) {
-    i = lastRow - 1
-    while (i > -1 && values[i][2 + 5 * k] === '') { i-- }
-
-    while (i > -1) {
-      if (values[i][2 + 5 * k] === '') {
-        list[n] = RangeUtils.rollA1Notation(5 + i, 3 + 5 * k)
-        n++
-      }
-      i--
-    }
-  }
-
-  if (list.length > 0) sheet.getRangeList(list).setValue(0)
-  SpreadsheetApp.flush()
-}
-
-function fillCardWithZeros (sheet, col) {
-  let lastRow
-  let i, k
-
-  lastRow = sheet.getLastRow()
-  if (lastRow < 6) return
-
-  lastRow -= 5
-  const values = sheet.getRange(6, col, lastRow, 18).getValues()
-  col += 3
-
-  let n = 0
-  const list = []
-
-  for (k = 0; k < 3; k++) {
-    i = lastRow - 1
-    while (i > -1 && values[i][3 + 6 * k] === '') { i-- }
-
-    while (i > -1) {
-      if (values[i][3 + 6 * k] === '') {
-        list[n] = RangeUtils.rollA1Notation(6 + i, col + 6 * k)
-        n++
-      }
-      i--
-    }
-  }
-
-  if (list.length > 0) sheet.getRangeList(list).setValue(0)
-  SpreadsheetApp.flush()
 }
